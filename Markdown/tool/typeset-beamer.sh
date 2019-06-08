64a264c7b33cbabf9302944a41240a2bf29dfb82
Justin Andresen <jan.dresen.95@gmail.com>
2019-06-08 12:04:24 +0200

    Add script to convert Markdown to Beamer
    The script can be used to create Beamer presentations from a Markdown
    document. The CAU Beamer theme is automatically downloaded.

---
+++ Markdown/tool/typeset-beamer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+script="$0"
+script_dir=$(dirname "$script")
+theme_dir=$(realpath "$script_dir/cau_beamertheme")
+
+# Clone CAU beamer theme if it does not exist.
+if ! [ -d "$theme_dir" ]; then
+  git clone https://cau-git.rz.uni-kiel.de/RZ/document_templates/latex/beamer.git "$theme_dir"
+fi
+
+# Make theme visible to LaTeX.
+export TEXINPUTS="$theme_dir:$TEXINPUTS"
+echo "$TEXINPUTS"
+
+# Forward all arguments and add beamer arguments.
+"$script_dir"/typeset.sh "$@" --to beamer --incremental
