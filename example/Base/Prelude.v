6550a931ffd09586848e09ea05fbb6a4f091c23b
Justin Andresen <jan.dresen.95@gmail.com>
2019-06-09 12:59:40 +0200

    Export bool and unit prelude libraries

--- example/Base/Prelude.v
+++ example/Base/Prelude.v
@@ -1,3 +1,5 @@
+From Base Require Export Prelude.Bool.
 From Base Require Export Prelude.Int.
 From Base Require Export Prelude.List.
 From Base Require Export Prelude.Pair.
+From Base Require Export Prelude.Unit.
