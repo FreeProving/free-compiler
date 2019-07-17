45e6fcb83245c17795fd38538684b7efb9265b6e
Justin Andresen <jan.dresen.95@gmail.com>
2019-07-17 22:47:13 +0200

    Remove unused code from `src`
    Haskell modules from the old implementation have been moved from
    the `src` directory to a new directory `old`. The modules did not
    compile anymore due to previous changes. For now we are keeping the
    files as a reference for the new implementation.

--- haskellToCoqCompiler/src/lib/Compiler/Language/Coq/Preamble.hs
+++ haskellToCoqCompiler/src/lib/Compiler/Language/Coq/Preamble.hs
@@ -9,22 +9,17 @@ import qualified Language.Coq.Gallina          as G
 
 import           Compiler.Util.Data.List.NonEmpty
                                                 ( toNonEmptyList )
-import           Compiler.Converter.Types       ( ConversionMonad(..) )
 
 -- | Generates import sentences for the Coq libraries used by the
 --   generated code.
 --
 --   The conversion monad is needed to import the right monad definitions.
-importDefinitions :: ConversionMonad -> [G.Sentence]
-importDefinitions cMonad = if cMonad == Option
-  then [stringImport, libraryImport, monadImport, optionImport]
-  else [stringImport, libraryImport, monadImport, identityImport]
+importDefinitions :: [G.Sentence]
+importDefinitions = [stringImport, libraryImport, monadImport]
  where
   stringImport   = requireImport [T.pack "String"]
   libraryImport  = requireImport [T.pack "ImportModules"]
   monadImport    = moduleImport [T.pack "Monad"]
-  optionImport   = moduleImport [T.pack "OptionDataTypes"]
-  identityImport = moduleImport [T.pack "IdentityDataTypes"]
 
 -- | Creates a "Require Import" sentence.
 requireImport :: [G.ModuleIdent] -> G.Sentence
