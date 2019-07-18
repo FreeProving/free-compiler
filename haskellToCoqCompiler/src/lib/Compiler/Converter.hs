FreeProving/free-compiler

commit 87e6566f3f0eb2f570d84218d5d63470e3ee9074

Remove non empty list utility module #36
The utility functions for non empty lists are defined in the module
`Data.List.NonEmpty` already.

haskellToCoqCompiler/src/lib/Compiler/Converter.hs
@@ -8,6 +8,7 @@ import           Control.Monad.Extra            ( concatMapM )
 import           Data.Maybe                     ( maybe
                                                 , catMaybes
                                                 )
+import           Data.List.NonEmpty            as NonEmpty
 
 import           Compiler.Analysis.DependencyAnalysis
 import           Compiler.Converter.State
@@ -19,7 +20,6 @@ import qualified Compiler.Language.Haskell.SimpleAST
 import           Compiler.Pretty
 import           Compiler.Reporter
 import           Compiler.SrcSpan
-import           Compiler.Util.Data.List.NonEmpty
 
 -- | Initially the environment contains the predefined functions, data types
 --   and their constructors from the Coq Base library that accompanies this
@@ -85,7 +85,7 @@ convertDataDecls dataDecls = do
   mapM_ defineDataDecl dataDecls
   (indBodies, extraSentences) <- mapAndUnzipM convertDataDecl dataDecls
   return
-    ( G.InductiveSentence (G.Inductive (toNonEmptyList indBodies) [])
+    ( G.InductiveSentence (G.Inductive (NonEmpty.toList indBodies) [])
     : concat extraSentences
     )
 
@@ -233,7 +233,7 @@ convertTypeVarDecls explicitness typeVarDecls
     return
       [ G.Typed G.Ungeneralizable
                 explicitness
-                (toNonEmptyList (map (G.Ident . G.bare) idents'))
+                (NonEmpty.toList (map (G.Ident . G.bare) idents'))
                 G.sortType
       ]
 
@@ -257,7 +257,7 @@ convertArg' ident' argType = do
   return
     (G.Typed G.Ungeneralizable
              G.Explicit
-             (singleton (G.Ident (G.bare ident')))
+             (return (G.Ident (G.bare ident')))
              argType'
     )
 
@@ -329,7 +329,7 @@ genericArgDecls :: G.Explicitness -> [G.Binder]
 genericArgDecls explicitness = map (uncurry genericArgDecl) CoqBase.freeArgs
  where
   genericArgDecl :: G.Qualid -> G.Term -> G.Binder
-  genericArgDecl = G.Typed G.Ungeneralizable explicitness . singleton . G.Ident
+  genericArgDecl = G.Typed G.Ungeneralizable explicitness . return . G.Ident
 
 -- | Smart constructor for the application of a Coq function or (type)
 --   constructor that requires the parameters for the @Free@ monad.

haskellToCoqCompiler/src/lib/Compiler/Language/Coq/AST.hs
@@ -20,10 +20,9 @@ module Compiler.Language.Coq.AST
 where
 
 import qualified Data.Text                     as T
+import qualified Data.List.NonEmpty            as NonEmpty
 import qualified Language.Coq.Gallina          as G
 import           Language.Coq.Gallina
-
-import           Compiler.Util.Data.List.NonEmpty
 
 -------------------------------------------------------------------------------
 -- Comments                                                                  --
@@ -61,9 +60,9 @@ unpackQualid (G.Qualified _ _) = Nothing
 --   to that term. Otherwise a new application term is created.
 app :: G.Term -> [G.Term] -> G.Term
 app (G.App func args) args' =
-  G.App func (args <> toNonEmptyList (map G.PosArg args'))
+  G.App func (args <> NonEmpty.toList (map G.PosArg args'))
 app func args | null args = func
-              | otherwise = G.App func (toNonEmptyList (map G.PosArg args))
+              | otherwise = G.App func (NonEmpty.toList (map G.PosArg args))
 
 -- | Smart constructor for a Coq function type.
 arrows
@@ -87,4 +86,4 @@ sortType = G.Sort G.Type
 -- | Creates a "From ... Require Import ..." sentence.
 requireImportFrom :: G.ModuleIdent -> [G.ModuleIdent] -> G.Sentence
 requireImportFrom library modules = G.ModuleSentence
-  (G.Require (Just library) (Just G.Import) (toNonEmptyList modules))
+  (G.Require (Just library) (Just G.Import) (NonEmpty.toList modules))

haskellToCoqCompiler/haskellToCoqCompiler.cabal
@@ -34,7 +34,6 @@ library haskell-to-coq-internal
                       , Compiler.Pretty.Coq
                       , Compiler.Reporter
                       , Compiler.SrcSpan
-                      , Compiler.Util.Data.List.NonEmpty
                       , Compiler.Util.Data.String
   build-depends:      base                ==4.12.*
                         -- Monad transformers:

haskellToCoqCompiler/src/lib/Compiler/Util/Data/List/NonEmpty.hs
@@ -1,14 +1,0 @@
-module Compiler.Util.Data.List.NonEmpty where
-
-import qualified GHC.Base                      as B
-
----------------------- NonemptyList Conversions
---convert single element to nonempty list
-singleton :: a -> B.NonEmpty a
-singleton a = a B.:| []
-
-toNonEmptyList :: [a] -> B.NonEmpty a
-toNonEmptyList (x : xs) = x B.:| xs
-
-fromNonEmptyList :: B.NonEmpty a -> [a]
-fromNonEmptyList (x B.:| xs) = x : xs

commit 2617cd193869227ef404a15422aca3f3739bc6c2

Fix compilation #36
Removing the non-empty list utility library in 87e6566f
introduced some type errors.

haskellToCoqCompiler/src/lib/Compiler/Converter.hs
@@ -8,7 +8,7 @@ import           Control.Monad.Extra            ( concatMapM )
 import           Data.Maybe                     ( maybe
                                                 , catMaybes
                                                 )
-import           Data.List.NonEmpty            as NonEmpty
+import qualified Data.List.NonEmpty            as NonEmpty
 
 import           Compiler.Analysis.DependencyAnalysis
 import           Compiler.Converter.State
@@ -84,7 +84,7 @@ convertDataDecls dataDecls = do
   mapM_ defineDataDecl dataDecls
   (indBodies, extraSentences) <- mapAndUnzipM convertDataDecl dataDecls
   return
-    ( G.InductiveSentence (G.Inductive (NonEmpty.toList indBodies) [])
+    ( G.InductiveSentence (G.Inductive (NonEmpty.fromList indBodies) [])
     : concat extraSentences
     )
 
@@ -232,7 +232,7 @@ convertTypeVarDecls explicitness typeVarDecls
     return
       [ G.Typed G.Ungeneralizable
                 explicitness
-                (NonEmpty.toList (map (G.Ident . G.bare) idents'))
+                (NonEmpty.fromList (map (G.Ident . G.bare) idents'))
                 G.sortType
       ]
 

haskellToCoqCompiler/src/lib/Compiler/Language/Coq/AST.hs
@@ -60,9 +60,9 @@ unpackQualid (G.Qualified _ _) = Nothing
 --   to that term. Otherwise a new application term is created.
 app :: G.Term -> [G.Term] -> G.Term
 app (G.App func args) args' =
-  G.App func (args <> NonEmpty.toList (map G.PosArg args'))
+  G.App func (args <> NonEmpty.fromList (map G.PosArg args'))
 app func args | null args = func
-              | otherwise = G.App func (NonEmpty.toList (map G.PosArg args))
+              | otherwise = G.App func (NonEmpty.fromList (map G.PosArg args))
 
 -- | Smart constructor for a Coq function type.
 arrows
@@ -86,4 +86,4 @@ sortType = G.Sort G.Type
 -- | Creates a "From ... Require Import ..." sentence.
 requireImportFrom :: G.ModuleIdent -> [G.ModuleIdent] -> G.Sentence
 requireImportFrom library modules = G.ModuleSentence
-  (G.Require (Just library) (Just G.Import) (NonEmpty.toList modules))
+  (G.Require (Just library) (Just G.Import) (NonEmpty.fromList modules))