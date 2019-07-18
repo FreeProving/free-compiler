885279593f82db00a3207748e9200bd7baece206
Justin Andresen <jan.dresen.95@gmail.com>
2019-07-18 12:23:00 +0200

    Actually remove monad utility module #36
    When I removed the imports of the monad utility module, I forgot
    to remove the Haskell file itself.

--- haskellToCoqCompiler/src/lib/Compiler/Util/Control/Monad.hs
+++
@@ -1,6 +0,0 @@
-module Compiler.Util.Control.Monad where
-
--- | Maps each element of a list to a monadic action, evaluates these
---   actions from left to right, and concatenates the resulting lists.
-concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
-concatMapM f xs = mapM f xs >>= return . concat
