a2e5e9cf5b5cc84dfaa65942279a98240717b608
Justin Andresen <jan.dresen.95@gmail.com>
2019-08-25 16:12:57 +0200

    Add script for spell checking #53

---
+++ LaTeX/tool/spell-check.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Formatted and colored output.
+bold=$(tput bold)
+reset=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+
+# Change into LaTeX directory.
+script="$0"
+script_dir=$(dirname "$script")
+cd "$script_dir/.."
+
+# Custom dictionary file that contains words to ignore.
+dictionary=".dictionary"
+
+# Spell check all LaTeX files in the current directory.
+error_code="0"
+files=$(find . -name "*.tex")
+for file in $files; do
+  echo -n "[  ] ${bold}${file}${reset}"
+  words=$(
+    cat "$file"                                             |
+
+    # Ignore minted code blocks.
+    tr '\n' ' '                                             |
+    sed -e 's/\\begin{minted}.*\end{minted}//g'             |
+
+    # Spell check.
+    aspell list                                             \
+           --lang=en                                        \
+           --mode=tex                                       \
+           --add-tex-command "autoref p"                    \
+           --add-tex-command "newcommand pop"               \
+           --add-tex-command "setminted p"                  \
+           --add-tex-command "newmintinline opp"            \
+           --add-tex-command "haskell p"                    \
+           --add-tex-command "cop p"                        |
+
+    # Remove duplicates.
+    sort                                                    |
+    uniq                                                    |
+
+    # Ignore words from dictionary file.
+    grep -v -w -f "$dictionary"
+  )
+
+  # Print misspelled words.
+  if [ -z "$words" ]; then
+    echo $'\r'"[${bold}${green}✓${reset}"
+  else
+    echo $'\r'"[${bold}${red}✕${reset}"
+    echo "$words" | sed -rn 's/^(.*)$/ - '"${red}"'\1'"${reset}"' /gp'
+    error_code="1"
+  fi
+done
+
+# Return falsy error code if there are misspelled words.
+exit "$error_code"
