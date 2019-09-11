ed7fe2ecbd9f6601bad6759ae78371127323dc8f
Justin Andresen <jan.dresen.95@gmail.com>
2019-09-11 12:54:52 +0200

    Build presentation in CI pipeline #62

--- Markdown/tool/typeset-revealjs.sh
+++ Markdown/tool/typeset-revealjs.sh
@@ -8,19 +8,19 @@ input_file="$1"
 if [ "$input_file" == "--watch" ]; then
   input_file="$2"
 fi
-if [ -z "$input_file" ]; then
+if [[ -z "$input_file" ]]; then
   echo "Usage: $0 [--watch] path/to/input-file.md [args]"
   exit 0
 fi
 input_dir=$(dirname "$input_file")
 
 # Install bower dependencies.
-if ! [ -d "$input_dir/bower_components" ]; then
+if ! [[ -d "$input_dir/bower_components" ]] && which bower; then
   owd=$(pwd)
   cd "$input_dir"
 
   # Create bower project if it does not exist.
-  if ! [ -f "bower.json" ]; then
+  if ! [[ -f "bower.json" ]]; then
     bower init
     bower install --save reveal.js
     bower install --save MathJax
