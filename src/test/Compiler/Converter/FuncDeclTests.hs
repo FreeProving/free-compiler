FreeProving/free-compiler

commit 78d26901415550eaa4e9b151a50a337ba96f1200

Allow ambigious imports of names that are not used #2

src/lib/Compiler/Environment.hs
@@ -10,7 +10,6 @@ module Compiler.Environment
   , Environment(..)
   , emptyEnv
   , childEnv
-  , isTopLevel
   -- * Module information
   , makeModuleAvailable
   , isModuleAvailable
@@ -25,6 +24,7 @@ module Compiler.Environment
   , defineDecArg
   , defineTypeSig
   -- * Looking up entries from the environment
+  , lookupEntries
   , lookupEntry
   , existsLocalEntry
   , isFunction
@@ -53,9 +53,7 @@ import           Data.Composition               ( (.:)
 import           Data.List                      ( find )
 import           Data.Map.Strict                ( Map )
 import qualified Data.Map.Strict               as Map
-import           Data.Maybe                     ( fromMaybe
-                                                , isJust
-                                                )
+import           Data.Maybe                     ( isJust )
 import           Data.Tuple.Extra               ( (&&&) )
 import           Control.Monad                  ( join )
 
@@ -103,10 +101,12 @@ data Environment = Environment
   , envAvailableModules :: Map HS.ModName Environment
     -- ^ Maps names of modules that can be imported to their environments.
 
-  , envEntries :: Map ScopedName (EnvEntry, Int)
+  , envEntries :: Map ScopedName ([EnvEntry], Int)
     -- ^ Maps Haskell names to entries for declarations.
     --   In addition to the entry, the 'envDepth' of the environment is
     --   recorded.
+    --   There can be multiple entries with the same name as long as they are
+    --   not referenced.
   , envTypeSigs :: Map HS.QName HS.Type
     -- ^ Maps names of Haskell functions to their annotated types.
   , envDecArgs :: Map HS.QName Int
@@ -144,10 +144,6 @@ emptyEnv = Environment
 childEnv :: Environment -> Environment
 childEnv env = env { envDepth = envDepth env + 1 }
 
--- | Tests whether the given environment has no parent environment.
-isTopLevel :: Environment -> Bool
-isTopLevel = (== 0) . envDepth
-
 -------------------------------------------------------------------------------
 -- Modules                                                                   --
 -------------------------------------------------------------------------------
@@ -174,7 +170,7 @@ lookupAvailableModule name = Map.lookup name . envAvailableModules
 exportedEntries :: Environment -> Map ScopedName EnvEntry
 exportedEntries =
   Map.filterWithKey (flip (const isUnQual))
-    . Map.map fst
+    . Map.map (head . fst)
     . Map.filter ((== 0) . snd)
     . envEntries
  where
@@ -227,9 +223,13 @@ addEntry name entry env = addEntry' name entry (envDepth env) env
 --   to record.
 addEntry' :: HS.QName -> EnvEntry -> Int -> Environment -> Environment
 addEntry' name entry depth env = env
-  { envEntries = Map.insert (entryScope entry, name)
-                            (entry           , depth)
-                            (envEntries env)
+  { envEntries = Map.insertWith
+                   (\(es1, d1) (es2, d2) ->
+                     if d2 == d1 then (es1 ++ es2, d1) else (es2, d2)
+                   )
+                   (entryScope entry, name)
+                   ([entry]         , depth)
+                   (envEntries env)
   }
 
 -- | Inserts the given type signature into the environment.
@@ -247,12 +247,18 @@ defineDecArg name index env =
 -- Looking up entries from the environment                                   --
 -------------------------------------------------------------------------------
 
--- | Looksup the entry with the given name in the specified scope of the
---   given environment.
---
---   Returns @Nothing@ if there is no such entry.
+-- | Looks up the entries that have been associated with the given name in
+--   the specified scope of the given environment.
+lookupEntries :: Scope -> HS.QName -> Environment -> [EnvEntry]
+lookupEntries scope name = maybe [] fst . Map.lookup (scope, name) . envEntries
+
+-- Like 'lookupEntries' but returns @Nothing@ if the given name is ambigous.
 lookupEntry :: Scope -> HS.QName -> Environment -> Maybe EnvEntry
-lookupEntry scope name = fmap fst . Map.lookup (scope, name) . envEntries
+lookupEntry = maybeFromSingleton .:. lookupEntries
+ where
+  maybeFromSingleton :: [a] -> Maybe a
+  maybeFromSingleton [x] = Just x
+  maybeFromSingleton _   = Nothing
 
 -- | Tests whether there is an entry with the given name in the current
 --   environment that was not inherited from a parent environment.
@@ -266,12 +272,11 @@ existsLocalEntry scope name =
 --
 --   Returns @False@ if there is no such function.
 isFunction :: HS.QName -> Environment -> Bool
-isFunction = fromMaybe False . fmap isFuncEntry .: lookupEntry ValueScope
+isFunction = maybe False isFuncEntry .: lookupEntry ValueScope
 
 -- | Test whether the variable with the given name is not monadic.
 isPureVar :: HS.QName -> Environment -> Bool
-isPureVar =
-  fromMaybe False . fmap (isVarEntry .&&. entryIsPure) .: lookupEntry ValueScope
+isPureVar = maybe False (isVarEntry .&&. entryIsPure) .: lookupEntry ValueScope
 
 -- | Looks up the Coq identifier for a Haskell function, (type)
 --   constructor or (type) variable with the given name.
@@ -292,7 +297,7 @@ lookupSmartIdent =
 -- | Gets a list of Coq identifiers for functions, (type/smart) constructors,
 --   (type/fresh) variables that were used in the given environment already.
 usedIdents :: Environment -> [G.Qualid]
-usedIdents = concatMap (entryIdents . fst) . Map.elems . envEntries
+usedIdents = concatMap (concatMap entryIdents . fst) . Map.elems . envEntries
  where
   entryIdents :: EnvEntry -> [G.Qualid]
   entryIdents entry
@@ -361,9 +366,7 @@ lookupTypeSig name = Map.lookup name . envTypeSigs
 --   Returns @False@ if there is no such function.
 isPartial :: HS.QName -> Environment -> Bool
 isPartial =
-  fromMaybe False
-    .  fmap (isFuncEntry .&&. entryIsPartial)
-    .: lookupEntry ValueScope
+  maybe False (isFuncEntry .&&. entryIsPartial) .: lookupEntry ValueScope
 
 -- | Looks up the index of the decreasing argument of the recursive function
 --   with the given name.

src/lib/Compiler/Converter/TypeDecl.hs
@@ -5,7 +5,6 @@ module Compiler.Converter.TypeDecl where
 
 import           Control.Monad                  ( mapAndUnzipM )
 import           Control.Monad.Extra            ( concatMapM )
-import qualified Data.Map.Strict               as Map
 import           Data.Maybe                     ( catMaybes )
 import           Data.List                      ( partition )
 import qualified Data.List.NonEmpty            as NonEmpty
@@ -33,32 +32,18 @@ import           Compiler.Monad.Instance.Fail   ( )
 convertTypeComponent
   :: DependencyComponent HS.TypeDecl -> Converter [G.Sentence]
 convertTypeComponent (NonRecursive decl)
-  | isTypeSynDecl decl = defineTypeDecl decl >> convertTypeSynDecl decl
+  | isTypeSynDecl decl = convertTypeSynDecl decl
   | otherwise          = convertDataDecls [decl]
 convertTypeComponent (Recursive decls) = do
   let (typeSynDecls, dataDecls) = partition isTypeSynDecl decls
   sortedTypeSynDecls <- sortTypeSynDecls typeSynDecls
-  dataDecls'         <- withTypeSynonyms sortedTypeSynDecls $ do
-    expandedDataDecls <- mapM expandAllTypeSynonymsInDecl dataDecls
-    convertDataDecls expandedDataDecls
+  expandedDataDecls  <- localEnv $ do
+    putEnv emptyEnv
+    mapM_ defineTypeDecl sortedTypeSynDecls
+    mapM expandAllTypeSynonymsInDecl dataDecls
+  dataDecls'    <- convertDataDecls expandedDataDecls
   typeSynDecls' <- concatMapM convertTypeSynDecl sortedTypeSynDecls
   return (dataDecls' ++ typeSynDecls')
-
--- | Creates a converter that runs the given converter in an environment that
---   contains only the type synonyms from the given type synonym
---   declarations.
---
---   The resulting environment contains both the old and the given type
---   synonym declarations.
-withTypeSynonyms :: [HS.TypeDecl] -> Converter a -> Converter a
-withTypeSynonyms typeSynDecls converter = do
-  (typeSyns, noTypeSyns) <-
-    inEnv $ Map.partition (isTypeSynEntry . fst) . envEntries
-  modifyEnv $ \env -> env { envEntries = noTypeSyns }
-  mapM_ defineTypeDecl typeSynDecls
-  x <- converter
-  modifyEnv $ \env -> env { envEntries = envEntries env `Map.union` typeSyns }
-  return x
 
 -- | Sorts type synonym declarations topologically.
 --
@@ -145,7 +130,8 @@ isTypeSynDecl (HS.DataDecl    _ _ _ _) = False
 
 -- | Converts a Haskell type synonym declaration to Coq.
 convertTypeSynDecl :: HS.TypeDecl -> Converter [G.Sentence]
-convertTypeSynDecl (HS.TypeSynDecl _ declIdent typeVarDecls typeExpr) =
+convertTypeSynDecl decl@(HS.TypeSynDecl _ declIdent typeVarDecls typeExpr) = do
+  defineTypeDecl decl
   localEnv $ do
     let ident = HS.fromDeclIdent declIdent
         name  = HS.UnQual (HS.Ident ident)

src/lib/Compiler/Environment/LookupOrFail.hs
@@ -6,11 +6,31 @@ module Compiler.Environment.LookupOrFail where
 
 import qualified Compiler.Coq.AST              as G
 import           Compiler.Environment
+import           Compiler.Environment.Entry
 import qualified Compiler.Haskell.AST          as HS
 import           Compiler.Haskell.SrcSpan
 import           Compiler.Monad.Converter
 import           Compiler.Monad.Reporter
 import           Compiler.Pretty
+
+-- | Looks up an entry of the environment with the given name or reports
+--   a fatal error message if the identifier has not been defined or the
+--   name is ambigious.
+--
+--   If an error is reported, it points to the given source span.
+lookupEntryOrFail :: SrcSpan -> Scope -> HS.QName -> Converter EnvEntry
+lookupEntryOrFail srcSpan scope name = do
+  entries <- inEnv $ lookupEntries scope name
+  case entries of
+    [entry] -> return entry
+    [] ->
+      reportFatal
+        $ Message srcSpan Error
+        $ ("Identifier not in scope '" ++ showPretty name ++ "'")
+    _ ->
+      reportFatal
+        $ Message srcSpan Error
+        $ ("Ambiguous occurrence '" ++ showPretty name ++ "'")
 
 -- | Looks up the Coq identifier for a Haskell function, (type)
 --   constructor or (type) variable with the given name or reports a fatal
@@ -23,13 +43,8 @@ lookupIdentOrFail
   -> HS.QName -- ^ The Haskell identifier to look up.
   -> Converter G.Qualid
 lookupIdentOrFail srcSpan scope name = do
-  mQualid <- inEnv $ lookupIdent scope name
-  case mQualid of
-    Just qualid -> return qualid
-    Nothing ->
-      reportFatal
-        $ Message srcSpan Error
-        $ ("Unknown identifier: " ++ showPretty name)
+  entry <- lookupEntryOrFail srcSpan scope name
+  return (G.bare (entryIdent entry))
 
 -- | Looks up the Coq identifier of a smart constructor of the Haskell
 --   data constructr with the given name or reports a fatal error message
@@ -41,13 +56,8 @@ lookupSmartIdentOrFail
   -> HS.QName -- ^ The Haskell identifier to look up.
   -> Converter G.Qualid
 lookupSmartIdentOrFail srcSpan name = do
-  mQualid <- inEnv $ lookupSmartIdent name
-  case mQualid of
-    Just qualid -> return qualid
-    Nothing ->
-      reportFatal
-        $ Message srcSpan Error
-        $ ("Unknown constructor: " ++ showPretty name)
+  entry <- lookupEntryOrFail srcSpan ValueScope name
+  return (G.bare (entrySmartIdent entry))
 
 -- | Looks up the annoated type of a user defined function with the given name
 --   and reports a fatal error message if there is no such type signature.
@@ -62,4 +72,4 @@ lookupTypeSigOrFail srcSpan ident = do
     Nothing ->
       reportFatal
         $ Message srcSpan Error
-        $ ("Missing type signature for " ++ showPretty ident)
+        $ ("Missing type signature for '" ++ showPretty ident ++ "'")

src/test/Compiler/Converter/ModuleTests.hs
@@ -33,6 +33,23 @@ testConvertImports = do
         _ <- convertTestModule ["module A where", "data A = A"]
         _ <- convertTestModule ["module B where", "import A", "type B = A"]
         return (return ())
+  it "allows ambigious imports of names that are not referenced"
+    $ shouldSucceed
+    $ fromModuleConverter
+    $ do
+        _ <- convertTestModule ["module A where", "foo :: ()", "foo = ()"]
+        _ <- convertTestModule ["module B where", "foo :: ()", "foo = ()"]
+        _ <- convertTestModule ["module C where", "import A", "import B"]
+        return (return ())
+  it "does not allow ambigious imports of names that are referenced"
+    $ shouldReportFatal
+    $ fromModuleConverter
+    $ do
+        _ <- convertTestModule ["module A where", "foo :: ()", "foo = ()"]
+        _ <- convertTestModule ["module B where", "foo :: ()", "foo = ()"]
+        convertTestModule
+          ["module C where", "import A", "import B", "bar :: ()", "bar = foo"]
+
   {- FIXME
   it "expands imported type synonyms correctly"
     $ shouldSucceed

commit 9d0cfa9e2d3740fd14d8682e2067257408c7cae2

Fix variable shadowing #10
This commit fixes a bug introduced by 78d26901 which
prevented variables to shadow existing variables.
Since I confused the arguments of `Map.insertWith` existing
variables effectively shadowed local variables.
Documentation and appropriate test cases have been added to
avoid this error in the future.

src/lib/Compiler/Environment.hs
@@ -171,14 +171,26 @@ addEntry name entry env = addEntry' name entry (envDepth env) env
 --   to record.
 addEntry' :: HS.QName -> EnvEntry -> Int -> Environment -> Environment
 addEntry' name entry depth env = env
-  { envEntries = Map.insertWith
-                   (\(es1, d1) (es2, d2) ->
-                     if d2 == d1 then (es1 `Set.union` es2, d1) else (es2, d2)
-                   )
-                   (entryScope entry   , name)
-                   (Set.singleton entry, depth)
-                   (envEntries env)
+  { envEntries = Map.insertWith mergeEntries
+                                (entryScope entry   , name)
+                                (Set.singleton entry, depth)
+                                (envEntries env)
   }
+ where
+  -- | Adds the given set of entries to an existing set of entries.
+  --
+  --   If the new entries are declared deeper, they shadow the existing
+  --   entries, i.e., the resulting set contains the new entries only.
+  --   If the new entries are declared at the same depth, they are added
+  --   to the existing entries. Both the existing and the new entries
+  --   remain visible.
+  mergeEntries
+    :: (Set EnvEntry, Int) -- ^ The new entries.
+    -> (Set EnvEntry, Int) -- ^ The old entries.
+    -> (Set EnvEntry, Int)
+  mergeEntries (newEntries, newDepth) (oldEntries, oldDepth)
+    | newDepth == oldDepth = (newEntries `Set.union` oldEntries, newDepth)
+    | otherwise            = (newEntries, newDepth)
 
 -- | Inserts the given type signature into the environment.
 defineTypeSig :: HS.QName -> HS.Type -> Environment -> Environment

src/test/Compiler/Converter/ExprTests.hs
@@ -273,6 +273,19 @@ testConvertCase = context "case expressions" $ do
           ++ "  | cons y ys => y"
           ++ "  end)"
 
+  it "allows case expressions to shadow local variables"
+    $ shouldSucceed
+    $ fromConverter
+    $ do
+        "e" <- defineTestVar "e"
+        "x" <- defineTestVar "x"
+        shouldTranslateExprTo "case e of { [] -> x; x : xs -> x }"
+          $  "e >>= (fun e_0 =>"
+          ++ "  match e_0 with"
+          ++ "  | nil => x"
+          ++ "  | cons x0 xs => x0"
+          ++ "  end)"
+
 -------------------------------------------------------------------------------
 -- Lambda abstractions                                                       --
 -------------------------------------------------------------------------------
@@ -293,6 +306,13 @@ testConvertLambda = context "lambda abstractions" $ do
     $ do
         "e" <- defineTestVar "e"
         shouldTranslateExprTo "\\x y -> e" "pure (fun x => pure (fun y => e))"
+
+  it "allows lambda abstractions to shadow local variables"
+    $ shouldSucceed
+    $ fromConverter
+    $ do
+        "x" <- defineTestVar "x"
+        shouldTranslateExprTo "\\x -> x" "pure (fun x0 => x0)"
 
 -------------------------------------------------------------------------------
 -- Integer expressions                                                       --

src/test/Compiler/Converter/FuncDeclTests.hs
@@ -95,6 +95,24 @@ testConvertNonRecFuncDecl =
             ++ "       | cons x xs' => x"
             ++ "       end)."
 
+    it "allows the function arguments to be shadowed"
+      $ shouldSucceed
+      $ fromConverter
+      $ do
+          shouldTranslateDeclsTo
+              [ "tail :: [a] -> [a]"
+              , "tail xs = case xs of { [] -> undefined; x : xs -> xs }"
+              ]
+            $  "Definition tail (Shape : Type) (Pos : Shape -> Type)"
+            ++ "  (P : Partial Shape Pos) {a : Type}"
+            ++ "  (xs : Free Shape Pos (List Shape Pos a))"
+            ++ "  : Free Shape Pos (List Shape Pos a)"
+            ++ " := xs >>= (fun xs_0 =>"
+            ++ "    match xs_0 with"
+            ++ "    | nil       => undefined"
+            ++ "    | cons x xs0 => xs0"
+            ++ "    end)."
+
 -------------------------------------------------------------------------------
 -- Recursive function declarations                                           --
 -------------------------------------------------------------------------------
@@ -333,3 +351,27 @@ testConvertRecFuncDecls =
             ++ " : Free Shape Pos a"
             ++ " := xs >>= (fun (xs_0 : List Shape Pos a) =>"
             ++ "      last_0 Shape Pos P xs_0)."
+
+    it "allows the arguments of helper functions to be shadowed"
+      $ shouldSucceed
+      $ fromConverter
+      $ do
+          shouldTranslateDeclsTo
+              [ "length :: [a] -> Integer"
+              , "length xs = case xs of { [] -> 0; x : xs -> length xs + 1 }"
+              ]
+            $  "(* Helper functions for length *) "
+            ++ "Fixpoint length_0 (Shape : Type) (Pos : Shape -> Type)"
+            ++ "  {a : Type} (xs : List Shape Pos a) {struct xs}"
+            ++ " := match xs with"
+            ++ "    | nil => pure 0%Z"
+            ++ "    | cons x xs0 => addInteger Shape Pos"
+            ++ "        (xs0 >>= (fun (xs0_0 : List Shape Pos a) =>"
+            ++ "             length_0 Shape Pos xs0_0))"
+            ++ "        (pure 1%Z)"
+            ++ "    end. "
+            ++ "Definition length (Shape : Type) (Pos : Shape -> Type) {a : Type}"
+            ++ "  (xs : Free Shape Pos (List Shape Pos a))"
+            ++ "  : Free Shape Pos (Integer Shape Pos)"
+            ++ " := xs >>= (fun (xs_0 : List Shape Pos a) =>"
+            ++ "    length_0 Shape Pos xs_0)."