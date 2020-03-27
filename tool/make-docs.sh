52acaef39825bc2d49712030f33e7f632e7d258e
Justin Andresen <jan.dresen.95@gmail.com>
2020-03-27 08:31:29 +0100

    Make CI pipeline fail when documentation could not be built

--- tool/make-docs.sh
+++ tool/make-docs.sh
@@ -6,5 +6,14 @@ script_dir=$(dirname "$script")
 root_dir=$(dirname "$script_dir")
 cd "$root_dir"
 
+# Make temporary file for Haddock output.
+haddock_output=$(mktemp)
+
 # Run tests with cabal.
-cabal new-haddock --haddock-hyperlink-source
+cabal new-haddock --haddock-hyperlink-source 2>&1 | tee "$haddock_output"
+
+# Cabal currently does not set the exit status correctly when the Haddock
+# command fails. Thus we have to set the exit status ourselves.
+if grep "Warning: Failed to build documentation for" "$haddock_output"; then
+  exit 1
+fi
