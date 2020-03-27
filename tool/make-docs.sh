01b7e89c1f46756806b08f70eff3776d8914a472
Justin Andresen <jan.dresen.95@gmail.com>
2020-03-27 11:14:39 +0100

    Print error message when documentation contains broken references

--- tool/make-docs.sh
+++ tool/make-docs.sh
@@ -6,6 +6,21 @@ script_dir=$(dirname "$script")
 root_dir=$(dirname "$script_dir")
 cd "$root_dir"
 
+# Colored output.
+red=$(tput setaf 1)
+bold=$(tput bold)
+reset=$(tput sgr0)
+
+# Helper function to print error messages.
+function printErrorAndDie() {
+  echo "${bold}"
+  echo "----------------------------------------------------------------------"
+  echo "${reset}"
+  echo "${red}${bold}Error:${reset}" \
+       "${bold}$1${reset}"
+  exit 1
+}
+
 # Make temporary file for Haddock output.
 haddock_output=$(mktemp)
 
@@ -15,5 +30,13 @@ cabal new-haddock --haddock-hyperlink-source 2>&1 | tee "$haddock_output"
 # Cabal currently does not set the exit status correctly when the Haddock
 # command fails. Thus we have to set the exit status ourselves.
 if grep "Warning: Failed to build documentation for" "$haddock_output"; then
-  exit 1
+  printErrorAndDie "Failed to build documentation"
+fi
+
+# Since there are many warnings that Haddock cannot find link destinations
+# for identifiers from external packages, print a more noticable error message
+# at the end if the documentation comments actually contain a reference to
+# an identifier that is not in scope.
+if grep -P "Warning: '\w*' is out of scope" "$haddock_output"; then
+  printErrorAndDie "The documentation contains out of scope identifiers"
 fi
