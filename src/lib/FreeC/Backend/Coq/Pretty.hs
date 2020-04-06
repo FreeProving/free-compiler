11ab423509b8808d664f7fb2412cfb85cbcd245d
Justin Andresen <jan.dresen.95@gmail.com>
2020-04-06 22:01:11 +0200

    Update type conversion tests

--- src/lib/FreeC/Backend/Coq/Pretty.hs
+++ src/lib/FreeC/Backend/Coq/Pretty.hs
@@ -1,11 +1,17 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 -- | This module contains a 'Pretty' instance for nodes of the Coq AST.
 --
 --   Since we want to have a pretty instance for all 'Gallina' instances
 --   but want to avoid overlapping instances, there is a wrapper type
---   'PrettyCoq'. To pretty print a node @x@ of the Coq AST writer
+--   'PrettyCoq'. To pretty print a node @x@ of the Coq AST you can write
 --   @pretty (PrettyCoq x)@.
+--
+--   There are instances for the actual node of commonly pretty printed types
+--   of nodes.
 module FreeC.Backend.Coq.Pretty where
 
+import qualified Language.Coq.Gallina          as G
 import           Language.Coq.Pretty            ( Gallina
                                                 , renderGallina
                                                 )
@@ -22,3 +28,7 @@ newtype PrettyCoq a = PrettyCoq { unwrapPrettyCoq :: a }
 instance Gallina a => Pretty (PrettyCoq a) where
   pretty     = renderGallina . unwrapPrettyCoq
   prettyList = prettySeparated (line <> line)
+
+-- | Terms often need to be pretty printed in the tests.
+instance Pretty G.Term where
+  pretty = pretty . PrettyCoq
