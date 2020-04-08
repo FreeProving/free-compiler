c758a4d1b2c7075cbb8abc889dc78bfb1b9a0d85
Justin Andresen <jan.dresen.95@gmail.com>
2020-04-09 01:59:02 +0200

    Add workflow to remove deployed documentation of deleted branches

---
+++ .github/scripts/slug.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script is used by workflows to "slug" arbitrary environment variables.
+# We have to "slug" names of branches such that it is save to use them in URLs.
+# The code for the function below has been copied from the following action.
+#
+#   https://github.com/rlespinasse/github-slug-action
+#
+# We've adapted their script to work with arbitrary environment variables.
+#
+#   usage: slug.sh <input-variable-contents> <output-variable-name>
+
+slug_ref() {
+    echo "$1" \
+         | tr "[:upper:]" "[:lower:]" \
+         | sed -r 's#refs/[^\/]*/##;s/[~\^]+//g;s/[^a-zA-Z0-9]+/-/g;s/^-+\|-+$//g;s/^-*//;s/-*$//' \
+         | cut -c1-63
+}
+
+echo "::set-env name=$2::$(slug_ref "$1")"
