27862929377f2e122f4823510715c08ff0289d53
Justin Andresen <jan.dresen.95@gmail.com>
2020-04-21 12:46:52 +0200

    Add installation script #11
    The installation script just invokes the `cabal new-install`
    command but also prints the exact version (i.e., the output
    of the `git describe --always --dirty` command) to a `VERSION`
    file. The `VERSION` file will be used by the `--version` option
    to print the exact version the compiler has been installed from.
    Until now the exact version was only printed when the compiler
    was executed without installation (i.e., `./tool/run.sh`) since
    Cabal installs local packages in a temporary directory where
    `githash` cannot access the `.git` directory.

---
+++ tool/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script install the compiler via the `cabal new-install` command.
+# The Cabal command cannot be used directly since we have to perform some
+# setup actions even before the sdist tarball is created by Cabal.
+#
+# All command line options that are passed to this script are forwarded to
+# Cabal such that the user can still customize the installation.
+
+# Change into the compiler's root directory.
+script=$(realpath $0)
+script_dir=$(dirname "$script")
+root_dir=$(dirname "$script_dir")
+cd "$root_dir"
+
+# Write the precise version of the compiler to a file in the root directory.
+if which git >/dev/null && [ -d .git ]; then
+  git describe --always --dirty > VERSION
+else
+  echo 'unknown' > VERSION
+fi
+
+# Clean up the `VERSION` file when installation is done or canceled, e.g., by
+# pressing `CTRL+C`.
+function cleanup() {
+  if [ -f VERSION ]; then
+    rm VERSION
+  fi
+}
+trap cleanup INT TERM EXIT
+
+# Install the compiler from the previously created source distribution archive.
+# If the compiler is installed already, it is reinstalled.
+cabal new-install --overwrite-policy=always "$@" freec
