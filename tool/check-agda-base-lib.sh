294f544c001a2393007342ecf370f4b0130401bf
jhoefer <jonas.hoefer99@gmail.com>
2020-05-26 12:15:38 +0200

    Fix script for checking agda base lib

--- tool/check-agda-base-lib.sh
+++ tool/check-agda-base-lib.sh
@@ -6,6 +6,10 @@ script_dir=$(dirname "$script")
 root_dir=$(dirname "$script_dir")
 cd "$root_dir"
 
+# change into the agda base-lib dir (location of `.agda-lib` file)
+cd base/agda
+
+# check all agda files
 for f in $(find . -name "*.agda")
   do agda "$f"
 done
