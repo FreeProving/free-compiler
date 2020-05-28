0079bc9ae26b3c66621a0ebf38258837cb9bb909
jhoefer <jonas.hoefer99@gmail.com>
2020-05-28 14:36:45 +0200

    Add some smart constructors for Agda AST nodes #29
    This commit adds some smart constructors for Agda AST nodes to easily test the pretty printer.

--- src/lib/FreeC/Backend/Agda/Syntax.hs
+++ src/lib/FreeC/Backend/Agda/Syntax.hs
@@ -3,7 +3,27 @@
 
 module FreeC.Backend.Agda.Syntax
   ( module Agda.Syntax.Concrete
+  , name
+  , intLiteral
+  , lambda
   )
 where
 
 import           Agda.Syntax.Concrete
+
+import qualified Agda.Syntax.Common            as Agda
+import qualified Agda.Syntax.Concrete          as Agda
+import qualified Agda.Syntax.Literal           as Agda
+import qualified Agda.Syntax.Position          as Agda
+
+
+name :: String -> Agda.Name
+name ident = Agda.Name Agda.NoRange Agda.InScope [Agda.Id ident]
+
+intLiteral :: Integer -> Agda.Expr
+intLiteral = Agda.Lit . Agda.LitNat Agda.NoRange
+
+lambda :: [Agda.Name] -> Agda.Expr -> Agda.Expr
+lambda args = Agda.Lam
+  Agda.NoRange
+  (Agda.DomainFree . Agda.defaultNamedArg . Agda.mkBinder_ <$> args)
