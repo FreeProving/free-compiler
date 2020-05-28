d820a91c0023ae7ab88a2f4285e412acca38edc7
jhoefer <jonas.hoefer99@gmail.com>
2020-05-28 11:29:49 +0200

    Add new `Module` exporting the Agda AST #27

---
+++ src/lib/FreeC/Backend/Agda/Syntax.hs
@@ -0,0 +1,9 @@
+-- | This module exports the original Agda AST to reduce coupling with the 
+--   Agda libraries.
+
+module FreeC.Backend.Agda.Syntax
+    ( module Agda.Syntax.Concrete
+    )
+where
+
+import           Agda.Syntax.Concrete
