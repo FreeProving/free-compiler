fb03529e21fe245649cd9c4bdca3c9ac0612cc43
jhoefer <jonas.hoefer99@gmail.com>
2020-05-28 11:38:18 +0200

    Remove a trailing whitepsace #27

--- src/lib/FreeC/Backend/Agda/Syntax.hs
+++ src/lib/FreeC/Backend/Agda/Syntax.hs
@@ -1,4 +1,4 @@
--- | This module exports the original Agda AST to reduce coupling with the 
+-- | This module exports the original Agda AST to reduce coupling with the
 --   Agda libraries.
 
 module FreeC.Backend.Agda.Syntax
