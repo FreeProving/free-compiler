a1003df4d200b370557b814a158d9d4e419c000c
jhoefer <jonas.hoefer99@gmail.com>
2020-05-28 17:11:07 +0200

    Add docs for (provisional) smart constructors #29

--- src/lib/FreeC/Backend/Agda/Syntax.hs
+++ src/lib/FreeC/Backend/Agda/Syntax.hs
@@ -3,26 +3,58 @@
 
 module FreeC.Backend.Agda.Syntax
   ( module Agda.Syntax.Concrete
+  , module Agda.Syntax.Common
+  , module Agda.Syntax.Position
   , name
   , intLiteral
+  , simpleImport
   , lambda
+  , qname
+  , qname'
   )
 where
 
 import           Agda.Syntax.Concrete
+import           Agda.Syntax.Position
+import           Agda.Syntax.Common
 
 import qualified Agda.Syntax.Common            as Agda
 import qualified Agda.Syntax.Concrete          as Agda
 import qualified Agda.Syntax.Literal           as Agda
 import qualified Agda.Syntax.Position          as Agda
 
+-- | Create a qualified name given a local variable 'Name' and a list of module
+--   'Name's.
+qname :: Agda.Name -> [Agda.Name] -> QName
+qname n = foldr Agda.Qual (Agda.QName n)
 
+-- | Creates a qualified name using an empty list of module names.
+qname' :: Agda.Name -> Agda.QName
+qname' = flip qname []
+
+-- | Creates an @open import@ declaration for the given qualified name.
+--
+--   @open import [modName]@
+simpleImport :: Agda.QName -> Agda.Declaration
+simpleImport modName = Agda.Import
+  Agda.NoRange
+  modName
+  Nothing
+  Agda.DoOpen
+  (Agda.ImportDirective Agda.NoRange Agda.UseEverything [] [] Nothing)
+
+-- | Creates a (not qualified) Agda variable name from a 'String'.
 name :: String -> Agda.Name
 name ident = Agda.Name Agda.NoRange Agda.InScope [Agda.Id ident]
 
+-- | Creates an integer literal.
 intLiteral :: Integer -> Agda.Expr
 intLiteral = Agda.Lit . Agda.LitNat Agda.NoRange
 
+-- | Creates a lambda expression, binding the given names and abstracting the
+--   given expression.
+--
+--   @λ x y … → [expr] @
 lambda :: [Agda.Name] -> Agda.Expr -> Agda.Expr
 lambda args = Agda.Lam
   Agda.NoRange
