5655433884c367570639a4fc7894643e0f68ed38
Marvin Lira <stu204529@mail.uni-kiel.de>
2020-05-30 16:46:26 +0200

    Adjust example coq file #18
    - Highlight different behavior of `Maybe` and `Error` on the second property.
    - Remove string scopes.
    - Improve names of lemmas.
    - Add newline at the end of the file.

--- example/Proofs/ConsUnconsProofs.v
+++ example/Proofs/ConsUnconsProofs.v
@@ -1,28 +1,48 @@
-From Base Require Import Free Free.Instance.Error Test.QuickCheck Prelude.
+From Base Require Import Free Free.Instance.Maybe Free.Instance.Error Test.QuickCheck Prelude.
 From Generated Require Import Proofs.ConsUncons.
 
-(* The proof for the QuickCheck property differs only in that way from proofs
-   about functions that do not return an error, that an [Partial] instance for
-   [Shape] and [Pos] is given. *)
+Require Import Coq.Logic.FunctionalExtensionality.
+
+(* The first QuickCheck property holds for any [Partial] instance. Therefore
+   its proof differs only in that way from proofs about functions that do not
+   return an error, that an [Partial] instance for [Shape] and [Pos] is given. *)
 Lemma cons_unconsE : quickCheck prop_cons_unconsE.
 Proof.
   intros Shape Pos Partial A fx fxs.
   reflexivity.
 Qed.
 
+(* The second QuickCheck property holds provable for the [Maybe] instance of [Partial]. *)
+Lemma unconsE_fst_Maybe : quickCheck (@prop_unconsE_fst Maybe.Shape Maybe.Pos Maybe.Partial).
+Proof.
+  intros A fxs.
+  simpl.
+  destruct fxs as [ xs | s pf ].
+  - destruct xs as [ | fx1 fxs1 ].
+    + simpl. unfold Nothing. f_equal. extensionality p. destruct p.
+    + reflexivity.
+  - simpl. f_equal. extensionality p. destruct p.
+Qed.
+
+(* But the second QuickCheck property doesn't hold for the [Error] instance of
+   [Partial] as [unconsE] and [head] have different error messages on an empty
+   list. *)
+Lemma unconsE_fst_Error : not (quickCheck (@prop_unconsE_fst (Error.Shape string) Error.Pos Error.Partial)).
+Proof.
+  intro H.
+  specialize (H bool (Nil _ _)).
+  discriminate H.
+Qed.
+
 Section ErrorMessages.
 
   (* To prove facts about the error messages we can write an abbreviation for
      an [error] with a specific message. *)
-  Open Scope string_scope.
-  Definition EmptyListError {A : Type} := @error _ _ _ A "uncons: empty list".
-  Close Scope string_scope.
+  Definition EmptyListError {A : Type} := @error _ _ _ A "uncons: empty list"%string.
 
   (* If we weren't looking for an actual [error] but for an [undefined] in haskell
      we could use the following definition. *)
-  Open Scope string_scope.
   Definition Undefined {A : Type} := @undefined _ _ _ A.
-  Close Scope string_scope.
 
   (* Now we can define and prove the lemma that using [unconsE] with an empty
      list results in an [EmptyListError] *)
@@ -36,7 +56,7 @@ Section ErrorMessages.
 
   (* We can also prove that using [unconsE] on an non-empty list does not cause
      an [EmptyListError]. *)
-  Lemma cons_uncons_no_empty_list_error : forall (A : Type) (fx : Free _ _ A) (fxs : Free _ _ (List _ _ A)),
+  Lemma cons_unconsE_no_empty_list_error : forall (A : Type) (fx : Free _ _ A) (fxs : Free _ _ (List _ _ A)),
     unconsE _ _ _ (Cons _ _ fx fxs) <> EmptyListError.
   Proof.
     intros A fx fxs.
@@ -46,7 +66,7 @@ Section ErrorMessages.
 
   (* And finally we can prove that an [EmptyListError] is the only error that
      can occur if the argument is error-free. *)
-  Lemma uncons_only_empty_list_error : forall (A : Type) (l : List _ _ A),
+  Lemma unconsE_only_empty_list_error : forall (A : Type) (l : List _ _ A),
     (exists (result : Pair _ _ A (List _ _ A)), unconsE _ _ _ (NoError l) = NoError result) \/
     (                                           unconsE _ _ _ (NoError l) = EmptyListError).
   Proof.
