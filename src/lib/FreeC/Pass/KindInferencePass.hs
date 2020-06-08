5c36a034d9288ee1e848437c47d30bc90b791243
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-06-08 11:27:10 +0200

    First try of implementation of pass #22

--- src/lib/FreeC/Pass/KindInferencePass.hs
+++ src/lib/FreeC/Pass/KindInferencePass.hs
@@ -3,15 +3,18 @@ module Freec.Pass.KindInferencePass
     ) where
 
 import qualified FreeC.IR.Syntax as IR
-
+import           FreeC.Monad.Converter
+import           FreeC.Monad.Reporter
+import           FreeC.Pass
+import           FreeC.Pretty
 
 kindInferencePass :: Pass IR.Module
 kindInferencePass = undefined
 
-containsLeftTypeVars :: IR.Type -> Bool
-containsLeftTypeVars (TypeVar _ _)               = False
-containsLeftTypeVars (TypeCon _ _)               = False
-containsLeftTypeVars (TypeApp _ (TypeVar _ _) _) = True
+containsLeftTypeVars :: IR.Type -> Converter ()
+containsLeftTypeVars (TypeVar _ _)               = return ()
+containsLeftTypeVars (TypeCon _ _)               = return ()
+containsLeftTypeVars (TypeApp _ (TypeVar _ _) _) = reportLeftVarError
 containsLeftTypeVars (TypeApp _ lhs _)           = containsLeftTypeVars lhs
 containsLeftTypeVars (FuncType _ lhs rhs)        = containsLeftTypeVars lhs ||
                                                    containsLeftTypeVars rhs
