41b709bc581d4c510ea6d709457611ec4345730d
Malte Clement <malteclement@web.de>
2020-06-08 21:17:16 +0200

    Add example to CompletePatternPass.hs #21

--- src/lib/FreeC/Pass/CompletePatternPass.hs
+++ src/lib/FreeC/Pass/CompletePatternPass.hs
@@ -1,5 +1,25 @@
+-- | This module contains a compiler pass that checks if all function
+--   declarations have complete patten macthing. A pattern ist complete if there
+--   is exactly one case alternative for each constructor of the corresponding
+--   type.
+--
+--   = Examples
+--
+--   == Example 1
+--   The following declarations
+--
+--   > data Maybe a = Just a | Nothing
+--   >
+--   > fromJust :: Maybe a -> a
+--   > fromJust @a (x :: Maybe a) = case x :: (Maybe a) of {Just a -> a}
+--
+--   should not pass the check.
+--
+-- TODO More examples
+
 module FreeC.Pass.CompletePatternPass
   ( completePatternPass
+    checkPatternFuncDecl
   )
 where
 
@@ -21,25 +41,26 @@ completePatternPass ast = do
   mapM_ checkPatternFuncDecl (IR.modFuncDecls ast)
   return ast
 
+-- | Checks a funcDecl for incomplete Patterns
 checkPatternFuncDecl :: IR.FuncDecl -> Converter ()
 checkPatternFuncDecl funcDecl = checkPatternExp (IR.funcDeclRhs funcDecl)
  where
-  checkPatternExp :: IR.Expr -> Converter ()
+  checkPatternExp :: IR.Expr -> Converter () -- Expr
   checkPatternExp (IR.Case srcSpan exprScrutinee exprAlts _) = do
     let mTau = IR.exprType (exprScrutinee)
-    case getTypeConName (fromJust mTau) of-- fromJust safe because type is known
+    case getTypeConName (fromJust mTau) of -- fromJust safe because type is known -- bind verwenden anstatt fromJust
       Nothing -> failedPatternCheck srcSpan
       Just typeName -> do
-        env <- getEnv
-        case lookupEntry IR.TypeScope typeName env of
+        env <- getEnv --inEnv == gets
+        case lookupEntry IR.TypeScope typeName env of -- type synonyms unchecked
           Just entry | isDataEntry entry -> do
             let typeConNames = entryConsNames entry
-            let altConNames = map (IR.conPatName . IR.altConPat) exprAlts
+                altConNames = map (IR.conPatName . IR.altConPat) exprAlts
             if (all (\x -> elem x typeConNames) typeConNames
                 && length typeConNames == length altConNames)
-              then return ()
-              else failedPatternCheck  srcSpan -- report fatal error
-          _   -> failedPatternCheck srcSpan  -- report fatal error
+              then return () -- when
+              else failedPatternCheck  srcSpan
+          _   -> failedPatternCheck srcSpan
   checkPatternExp (IR.App _ lhr rhs _) = checkPatternExp lhr >> checkPatternExp rhs
   checkPatternExp (IR.TypeAppExpr _ lhr _ _) = checkPatternExp lhr
   checkPatternExp (IR.If _ exprCond exprThen exprElse _) =
@@ -58,7 +79,8 @@ checkPatternFuncDecl funcDecl = checkPatternExp (IR.funcDeclRhs funcDecl)
   stringFromName (IR.Symbol s) = s
 
   getTypeConName :: IR.Type -> Maybe IR.TypeConName
-  getTypeConName (IR.TypeCon  _ typeConName) = Just typeConName
+  getTypeConName (IR.TypeCon  _ typeConName)   = Just typeConName
   getTypeConName (IR.TypeApp  _ typeAppLhs _ ) = getTypeConName typeAppLhs
     -- The type of the scrutinee shouldn't be function or a type var
-  getTypeConName _ = Nothing
+  getTypeConName IR.TypeVar{} = Nothing
+  getTypeConName IR.FuncType{} = Nothing
