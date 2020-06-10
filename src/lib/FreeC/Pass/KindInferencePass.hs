8a7b110cb30d5ee445f25228be57b7b6a40ae1e3
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-06-11 01:44:15 +0200

    Add documentation #22

--- src/lib/FreeC/Pass/KindInferencePass.hs
+++ src/lib/FreeC/Pass/KindInferencePass.hs
@@ -1,3 +1,52 @@
+
+-- | This module contains a compiler pass that checks if all type constructors
+--   in type expressions are fully applied and type variables are not applied.
+--
+--   For simplification, all type variables have kind @*@. Therefore, all type
+--   constructors must be fully applied, so partial application of type
+--   constructors is not allowed. This pass also checks that type variables and
+--   function types do not occur on the left hand side of a type application.
+--   If a type expression is found that does not match this rules, a fatal
+--   error is thrown. For simplification, a type expression that satisfies all
+--   of these rules is called kind-correct.
+--
+--   = Example
+--
+--   The following module contains three invalid definitions
+--
+--   > module Failures where
+--   >
+--   > data MyList a = MyNil | MyCons a (MyList a)
+--   >
+--   > fail1 :: MyList (MyList a a) -> Integer
+--   > fail1 x = 42
+--   >
+--   > fail2 :: a a -> Integer
+--   > fail2 x = 42
+--   >
+--   > fail3 :: MyList -> Integer
+--   > fail3 x = 42
+--
+--   = Specification
+--
+--   == Preconditions
+--
+--   The environment must contain all type declarations that are used in the
+--   module
+--
+--   == Translations
+--
+--   The AST is not changed. It is checked if all type constructor applications
+--   have the right number of arguments. It is also checked whether there are
+--   type variables or function types on the left-hand side of type
+--   applications. In these cases, a fatal error is thrown. Otherwise, the
+--   pass finishes with the given module unchanged.
+--
+--   == Postcondition
+--
+--   All type constructors in type expressions are applied to the correct
+--   number of arguments and there are no type variables or function types on
+--   the left-hand side of type applications. 
 module FreeC.Pass.KindInferencePass
   ( kindInferencePass
   )
@@ -13,6 +62,9 @@ import           FreeC.Monad.Reporter
 import           FreeC.Pass
 import           FreeC.Pretty                   ( showPretty )
 
+-- | A compiler pass that checks wheter there are type applications with type
+--   variables or function types on the left-hand side in the module and checks
+--   whether all type construcors in the module are fully applied.
 kindInferencePass :: Pass IR.Module
 kindInferencePass m@(IR.Module _ _ _ typeDecls typeSigs _ funcDecls) = do
   checkTypeDecls typeDecls
@@ -20,36 +72,48 @@ kindInferencePass m@(IR.Module _ _ _ typeDecls typeSigs _ funcDecls) = do
   checkFuncDecls funcDecls
   return m
 
+-- |
 checkMaybeTypeSchema :: Maybe IR.TypeSchema -> Converter ()
 checkMaybeTypeSchema Nothing  = return ()
 checkMaybeTypeSchema (Just x) = checkTypeSchema x
 
+-- | Checks whether an optional type expression is kind correct in case the
+--   type expression is present
 checkMaybeType :: Maybe IR.Type -> Converter ()
 checkMaybeType Nothing  = return ()
 checkMaybeType (Just x) = checkType x
 
+-- | Checks whether all type declarations have kind-correct type expressions
 checkTypeDecls :: [IR.TypeDecl] -> Converter ()
 checkTypeDecls = mapM_ checkTypeDecl
 
+-- | Checks whether all type signatures have kind-correct types
 checkTypeSigs :: [IR.TypeSig] -> Converter ()
 checkTypeSigs = mapM_ checkTypeSig
 
+-- | Checks whether all function declarations have kind-correct type
+--   annotations
 checkFuncDecls :: [IR.FuncDecl] -> Converter ()
 checkFuncDecls = mapM_ checkFuncDecl
 
+-- | Checks whether all type expressions in a type declaration are correct
 checkTypeDecl :: IR.TypeDecl -> Converter ()
 checkTypeDecl (IR.DataDecl    _ _ _ conDecls) = mapM_ checkConDecl conDecls
 checkTypeDecl (IR.TypeSynDecl _ _ _ typ     ) = checkType typ
 
+-- | Checks whether the arguments of a constructor declaration are kind-correct
 checkConDecl :: IR.ConDecl -> Converter ()
 checkConDecl (IR.ConDecl _ _ types) = mapM_ checkType types
 
+-- | Checks whether the type schema in a type signature is kind-correct
 checkTypeSig :: IR.TypeSig -> Converter ()
 checkTypeSig (IR.TypeSig _ _ typeSchema) = checkTypeSchema typeSchema
 
+-- | Checks whether a given type schema is kind-correct
 checkTypeSchema :: IR.TypeSchema -> Converter ()
 checkTypeSchema (IR.TypeSchema _ _ typ) = checkType typ
 
+-- | Checks whether a function declaration has kind-correct type annotations
 checkFuncDecl :: IR.FuncDecl -> Converter ()
 checkFuncDecl (IR.FuncDecl _ _ _ varPats retType rhs) = do
   checkVarPats varPats
@@ -57,6 +121,7 @@ checkFuncDecl (IR.FuncDecl _ _ _ varPats retType rhs) = do
   checkExpr rhs
   return ()
 
+-- | Checks whether all type annotations in an expression are kind-correct
 checkExpr :: IR.Expr -> Converter ()
 checkExpr (IR.Con _ _ typeSchema      ) = checkMaybeTypeSchema typeSchema
 checkExpr (IR.Var _ _ typeSchema      ) = checkMaybeTypeSchema typeSchema
@@ -85,27 +150,34 @@ checkExpr (IR.Lambda _ args rhs typeSchema) = do
   checkExpr rhs
   checkMaybeTypeSchema typeSchema
 
+-- | Performs @checkAlt@ on all elements of the given list.
 checkAlts :: [IR.Alt] -> Converter ()
 checkAlts = mapM_ checkAlt
 
+-- | Checks whether the variable patterns have correct type annotations and the
+--   expression has kind-correct type annotations
 checkAlt :: IR.Alt -> Converter ()
 checkAlt (IR.Alt _ _ varPats rhs) = do
   checkVarPats varPats
   checkExpr rhs
 
+-- | Performs @checkVarPat@ on all elems in the given list.
 checkVarPats :: [IR.VarPat] -> Converter ()
 checkVarPats = mapM_ checkVarPat
 
+-- | Checks whether the type annotation of a variable pattern is kind-correct.
 checkVarPat :: IR.VarPat -> Converter ()
 checkVarPat (IR.VarPat _ _ typ) = checkMaybeType typ
 
+-- | Checks whether a type is kind-correct
 checkType :: IR.Type -> Converter ()
 checkType = checkCorrectArities
 
-
+-- | Checks if all type constructors have the correct number of arguments
 checkCorrectArities :: IR.Type -> Converter ()
-checkCorrectArities = checkCorrectArities' 0 -- to be implemented
+checkCorrectArities = checkCorrectArities' 0
 
+-- | Helper function for @checkCorrectArities@
 checkCorrectArities' :: Int -> IR.Type -> Converter ()
 checkCorrectArities' depth (IR.TypeVar srcSpan varId) =
   when (depth /= 0)
