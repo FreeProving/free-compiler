c355b2ecc233b6b3a90cb530675e14a1055580c6
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-06-15 02:00:25 +0200

    Add inlining tests #7

--- src/lib/FreeC/IR/Inlining.hs
+++ src/lib/FreeC/IR/Inlining.hs
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
--- | This module contains functions for inlining the definition of
---   functions into expressions or other function declarations.
---
---   Inlining is performed during the translation of recursive function
---   declarations to inline the definition of the non-recursive main
---   function into the recursive helper functions.
-=======
-<<<<<<< HEAD
--- | This module contains functions for inlining the definition of
---   functions into expressions or other function declarations.
---
---   Inlining is performed during the translation of recursive function
---   declarations to inline the definition of the non-recursive main
---   function into the recursive helper functions.
-=======
 -- | This module contains functions for inlining the definition of
 --   functions into expressions or other function declarations.
 --
 --   Inlining is performed during the translation of recursive function
 --   declarations to inline the definition of the non-recursive main
 --   function into the recursive helper functions.
->>>>>>> 78f9ab89e8333f6eb7d7b10e57f553175ff93607
->>>>>>> issue-7
 
 module FreeC.IR.Inlining where
 
@@ -37,30 +19,30 @@ import           FreeC.Monad.Reporter
 import           FreeC.Pretty
 
 
--- | Inlines the right hand sides of the given function declarations into
---   the right hand sides of other function declarations.
+-- | Inlines the right hand sides of the given function declarations into
+--   the right hand sides of other function declarations.
 inlineFuncDecls :: [IR.FuncDecl] -> IR.FuncDecl -> Converter IR.FuncDecl
 inlineFuncDecls decls decl = do
   let rhs = IR.funcDeclRhs decl
   rhs' <- inlineExpr decls rhs
   return decl { IR.funcDeclRhs = rhs' }
 
--- | Inlines the right hand sides of the given function declarations into an
---   expression. This step is repeated until the expression remains unchanged
+-- | Inlines the right hand sides of the given function declarations into an
+--   expression. This step is repeated until the expression remains unchanged
 --   or no more function declarations are available.
 --   That is done under the assumption that regarding a certain position
 --   of the given expression every given function should be inlined at
---   most once in order to avoid infinit inlining.
+--   most once in order to avoid endless inlining.
 inlineExpr :: [IR.FuncDecl] -> IR.Expr -> Converter IR.Expr
 inlineExpr []    = return
 inlineExpr decls = inlineAndBind
  where
-  -- | Maps the names of function declarations in 'decls' to the arguments
-  --   and right hand sides of the functions.
+  -- | Maps the names of function declarations in 'decls' to the arguments
+  --   and right hand sides of the functions.
   declMap :: Map IR.QName ([IR.TypeVarDecl], [IR.VarPat], IR.Expr)
   declMap = foldr insertFuncDecl Map.empty decls
 
-  -- | Inserts a function declaration into 'declMap'.
+  -- | Inserts a function declaration into 'declMap'.
   insertFuncDecl
     :: IR.FuncDecl
     -> Map IR.QName ([IR.TypeVarDecl], [IR.VarPat], IR.Expr)
@@ -72,28 +54,24 @@ inlineExpr decls = inlineAndBind
     , IR.funcDeclRhs funcDecl
     )
 
-  -- | Applies 'inlineExpr'' on the given expression and wraps the result with
-  --   lambda abstractions for the remaining arguments.
-  --
-  --   It is an error if not all type arguments of an inlined function are
-  --   bound by visible type application expressions.
+  -- | Applies 'inlineExpr'' on the given expression and wraps the result with
+  --   lambda abstractions for the remaining arguments.
+  --
+  --   It is an error if not all type arguments of an inlined function are
+  --   bound by visible type application expressions.
   inlineAndBind :: IR.Expr -> Converter IR.Expr
   inlineAndBind expr = do
     (remainingArgs, expr') <- inlineVisiblyApplied expr
     if null remainingArgs
-    --(filter ((name /=) . IR.funcDeclQName) decls)--
-    --inlineExpr decls expr'
-      then if expr == expr'
-        then return expr'
-        else inlineExpr (filter ((name /=) . IR.funcDeclQName) decls) expr'
+      then return expr'
       else do
         let remainingArgPats = map IR.toVarPat remainingArgs
         return (IR.Lambda NoSrcSpan remainingArgPats expr' Nothing)
 
-  -- | Applies 'inlineExpr'' on the given expression and reports an
-  --   internal fatal error if not all type arguments have been
-  --   applied visibly.
-  inlineVisiblyApplied :: IR.Expr -> Converter ([String], IR.Expr, IR.QName)
+  -- | Applies 'inlineExpr'' on the given expression and reports an
+  --   internal fatal error if not all type arguments have been
+  --   applied visibly.
+  inlineVisiblyApplied :: IR.Expr -> Converter ([String], IR.Expr)
   inlineVisiblyApplied e = do
     (remainingTypeArgs, remainingArgs, e') <- inlineExpr' e
     unless (null remainingTypeArgs)
@@ -104,19 +82,18 @@ inlineExpr decls = inlineAndBind
       ++ " type arguments in an application of '"
       ++ showPretty e
       ++ "'."
-    return (remainingArgs, e', name)
+    return (remainingArgs, e')
 
-  -- | Performs inlining on the given subexpression.
-  --
-  --   If a function is inlined, fresh free variables are introduced for the
-  --   function arguments. The first two components of the returned tuple
-  --   contain the names of the type variables and variables that still need
-  --   to be bound. The last component of the returndes tuple is the qualified
-  --   name of the inlined function.
-  --   Function application and visible type application expressions
-  --   automatically substitute the corresponding argument for
-  --   the passed value.
-  inlineExpr' :: IR.Expr -> Converter ([String], [String], IR.Expr, IR.QName)
+  -- | Performs inlining on the given subexpression.
+  --
+  --   If a function is inlined, fresh free variables are introduced for the
+  --   function arguments. The first two components of the returned tuple
+  --   contain the names of the type variables and variables that still need
+  --   to be bound.
+  --   Function application and visible type application expressions
+  --   automatically substitute the corresponding argument for
+  --   the passed value.
+  inlineExpr' :: IR.Expr -> Converter ([String], [String], IR.Expr)
   inlineExpr' var@(IR.Var _ name _) = case Map.lookup name declMap of
     Nothing                    -> return ([], [], var)
     Just (typeArgs, args, rhs) -> do
@@ -124,14 +101,10 @@ inlineExpr decls = inlineAndBind
       (args', rhs'') <- renameArgs args rhs'
       rhs''' <- inlineExpr (filter ((name /=) . IR.funcDeclQName) decls) rhs''
       return
-        ( map IR.typeVarDeclIdent typeArgs'
-        , map IR.varPatIdent      args'
-        , rhs''
-        , name
-        )
+        (map IR.typeVarDeclIdent typeArgs', map IR.varPatIdent args', rhs''')
 
-  -- Substitute argument of inlined function and inline recursively in
-  -- function arguments.
+  -- Substitute argument of inlined function and inline recursively in
+  -- function arguments.
   inlineExpr' (IR.App srcSpan e1 e2 exprType) = do
     (remainingArgs, e1') <- inlineVisiblyApplied e1
     e2'                  <- inlineAndBind e2
@@ -142,7 +115,7 @@ inlineExpr decls = inlineAndBind
             e1''  = applySubst subst e1'
         return ([], remainingArgs', e1'')
 
-  -- Substitute type arguments of inlined function.
+  -- Substitute type arguments of inlined function.
   inlineExpr' (IR.TypeAppExpr srcSpan e t exprType) = do
     (remainingTypeArgs, remainingArgs, e') <- inlineExpr' e
     case remainingTypeArgs of
@@ -152,7 +125,7 @@ inlineExpr decls = inlineAndBind
             e''   = applySubst subst e'
         return (remainingTypeArgs', remainingArgs, e'')
 
-  -- Inline recursively.
+  -- Inline recursively.
   inlineExpr' (IR.If srcSpan e1 e2 e3 exprType) = do
     e1' <- inlineAndBind e1
     e2' <- inlineAndBind e2
@@ -167,18 +140,15 @@ inlineExpr decls = inlineAndBind
       expr' <- inlineAndBind expr
       return ([], [], IR.Lambda srcSpan varPats expr' exprType)
 
-  -- All other expressions remain unchanged.
-  inlineExpr' expr@(IR.Con _ _ _       ) = return ([], [], expr, emptyName)
-  inlineExpr' expr@(IR.Undefined _ _   ) = return ([], [], expr, emptyName)
-  inlineExpr' expr@(IR.ErrorExpr  _ _ _) = return ([], [], expr, emptyName)
-  inlineExpr' expr@(IR.IntLiteral _ _ _) = return ([], [], expr, emptyName)
+  -- All other expressions remain unchanged.
+  inlineExpr' expr@(IR.Con _ _ _       ) = return ([], [], expr)
+  inlineExpr' expr@(IR.Undefined _ _   ) = return ([], [], expr)
+  inlineExpr' expr@(IR.ErrorExpr  _ _ _) = return ([], [], expr)
+  inlineExpr' expr@(IR.IntLiteral _ _ _) = return ([], [], expr)
 
-  -- | Performs inlining on the right hand side of the given @case@-expression
-  --   alternative.
+  -- | Performs inlining on the right hand side of the given @case@-expression
+  --   alternative.
   inlineAlt :: IR.Alt -> Converter IR.Alt
   inlineAlt (IR.Alt srcSpan conPat varPats expr) = shadowVarPats varPats $ do
     expr' <- inlineAndBind expr
     return (IR.Alt srcSpan conPat varPats expr')
-
-  --constant used to express a dummy value for QName
-  emptyName = IR.UnQual (IR.Ident [])
