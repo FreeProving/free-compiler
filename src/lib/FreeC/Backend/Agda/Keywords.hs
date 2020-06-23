e1d95ef8591ea925c6b5494a369a417495d534a4
jhoefer <jonas.hoefer99@gmail.com>
2020-06-23 12:30:51 +0200

    Fix Agda `renamer` by reserving Agda keywords
    The renamer for Agda identifiers did not rename identifiers with the same name
    as a keyword in Agda. This commit adds a list of Agda keywords
    (`FreeC.Backend.Agda.Keywords`) and adds checks for them to the `renamer`.

---
+++ src/lib/FreeC/Backend/Agda/Keywords.hs
@@ -0,0 +1,56 @@
+-- | This module exports lists for keywords of the Agda language.
+
+module FreeC.Backend.Agda.Keywords where
+
+
+-- | Identifiers that cannot be used in Agda, because they are keywords
+--
+--   This list contains all keywords from the Agda docs
+--   <https://agda.readthedocs.io/en/v2.6.1/language/lexical-structure.html#keywords-and-special-symbols>.
+agdaKeywords :: [String]
+agdaKeywords =
+  [ "abstract"
+  , "constructor"
+  , "data"
+  , "do"
+  , "eta-equality"
+  , "field"
+  , "forall"
+  , "hiding"
+  , "import"
+  , "in"
+  , "inductive"
+  , "infix"
+  , "infixl"
+  , "infixr"
+  , "instance"
+  , "let"
+  , "macro"
+  , "module"
+  , "mutual"
+  , "no-eta-equality"
+  , "open"
+  , "overlap"
+  , "pattern"
+  , "postulate"
+  , "primitive"
+  , "private"
+  , "public"
+  , "quote"
+  , "quoteContext"
+  , "quoteGoal"
+  , "quoteTerm"
+  , "record"
+  , "renaming"
+  , "rewrite"
+  , "Set"
+  , "syntax"
+  , "tactic"
+  , "unquote"
+  , "unquoteDecl"
+  , "unquoteDef"
+  , "using"
+  , "variable"
+  , "where"
+  , "with"
+  ]
