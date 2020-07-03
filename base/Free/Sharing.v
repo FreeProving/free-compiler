51886d3ae695ee32dc956543dd2ab525b76cdc98
Maja <stu126115@mail.uni-kiel.de>
2020-07-03 11:09:28 +0200

    Define non-functional sharing operator for generic effects #77

--- base/Free/Sharing.v
+++ base/Free/Sharing.v
@@ -1,57 +1,19 @@
-From Base Require Import Free.Instance.Comb.
-From Base Require Import Free.Instance.Share.
-From Base Require Import Free.Instance.State.
 From Base Require Import Free.Monad.
 
-(* For testing *)
-From Base Require Import Free.Instance.Identity.
-From Base Require Import Prelude.
-
-(* A shape with the Share and State effect shapes wrapped around it. *)
-Definition SShape (Shape : Type) : Type := Comb.Shape State.Shape (Comb.Shape Share.Shape Shape).
-
-(* A position function with the Share and State effect position functions wrapped around it. *)
-Definition SPos {Shape : Type} (Pos : Shape -> Type) : SShape Shape -> Type := 
-  Comb.Pos State.Pos (Comb.Pos Share.Pos Pos).
-
-(* The type of a shared variable or expression of type Free Shape Pos A*)
-Definition Prog (Shape : Type) (Pos : Shape -> Type) (A : Type) := Free (SShape Shape) (SPos Pos) A.
-
-(* An effect-generic (non-functional) sharing operator for an already lifted variable or expression. *)
-(* Will eventually also require A to be an instance of Shareable. *)
+(** An effect-generic (non-functional) sharing operator.
+    The final operator will not be able to operate on arbitrary 
+    shapes and position functions but will require the state and 
+    sharing effects to be present.
+    The operator will eventually also require A to be an instance 
+    of a type class Shareable. *)
 Definition share {A : Type} {Shape : Type} {Pos : Shape -> Type}
-  (x : Prog Shape Pos A) 
-  : Prog Shape Pos (Prog Shape Pos A) := 
-  pure x.
+  (p : Free Shape Pos A) 
+  : Free Shape Pos (Free Shape Pos A) := 
+  pure p.
 
-(* A function to lift a Free value into the sharing setting. *)
-Definition liftS {A : Type} {Shape : Type} {Pos : Shape -> Type} (x : Free Shape Pos A) 
-  : Prog Shape Pos A :=
-  Comb.Monad.Inr (Comb.Monad.Inr x).
-
-(* An effect-generic (non-functional) sharing operator for a non-lifted variable or expression. *)
-(* Will eventually also require A to be an instance of Shareable. *)
-(* I'm not sure if it's going to be useful at all. *)
-Definition share' {A : Type} {Shape : Type} {Pos : Shape -> Type} (x : Free Shape Pos A) 
-  : Prog Shape Pos (Prog Shape Pos A) :=
-  pure (liftS x).
-
-
-(* Testing *)
-
-Definition n : Integer Identity.Shape Identity.Pos := 1%Z.
-
-(* Doubling an integer without sharing. *)
-Definition double {Shape : Type} {Pos : Shape -> Type} (x : Free Shape Pos (Integer Shape Pos)) := 
-   addInteger Shape Pos x x.
-
-Compute double (pure n).
-
-(* Doubling an integer that is shared. *)
-(* Note that we need to wrap the shape and position function with SShape and SPos. *)
-Definition doubleShare {Shape : Type} {Pos : Shape -> Type} 
-  (x : Prog Shape Pos (Integer Shape Pos))
-  : Prog Shape Pos (Integer Shape Pos) := 
-  share x >>= fun sx => addInteger (SShape Shape) (SPos Pos) sx sx.
-
-Compute doubleShare (liftS (pure n)).
+(** An effect-generic (non-functional) sharing operator that models 
+    call-by-value instead of call-by-name, that is, it evaluates its
+    program argument immediately. *)
+Definition share' {A : Type} {Shape : Type} {Pos : Shape -> Type} (p : Prog Shape Pos A) 
+  : Free Shape Pos (Free Shape Pos A) :=
+  p >>= fun x => pure (pure x).
