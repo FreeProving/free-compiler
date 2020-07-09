56603fe73561346b2468b6eb23881910085fbc5c
Maja <stu126115@mail.uni-kiel.de>
2020-07-09 10:50:16 +0200

    Add documentation to cbneed and make spacing consistent #115

--- base/Free/Malias.v
+++ base/Free/Malias.v
@@ -5,24 +5,25 @@ From Base Require Import Free.
 From Base Require Export Free.Instance.Comb.
 From Base Require Export Free.Instance.Share.
 
-(** An operator to model call-by-value evaluation *)
+(* An operator to model call-by-value evaluation *)
 Definition cbv {A : Type} {Shape : Type} {Pos : Shape -> Type} (p : Free Shape Pos A) 
   : Free Shape Pos (Free Shape Pos A) :=
   p >>= fun x => pure (pure x).
 
-(** An operator to model call-by-name evaluation *)
+(* An operator to model call-by-name evaluation *)
 Definition cbn {A : Type} {Shape : Type} {Pos : Shape -> Type}
   (p : Free Shape Pos A) 
   : Free Shape Pos (Free Shape Pos A) := 
   pure p.
 
+(* An operator to model call-by-need evaluation *)
 Definition cbneed {A : Type} {Shape : Type} {Pos : Shape -> Type}
   `{Injectable Share.Shape Share.Pos Shape Pos} (p : Free Shape Pos A)
   : Free Shape Pos (Free Shape Pos A) :=
   Get >>= fun '(i,j) =>
-  Put (i + 1, j) >>
+  Put (i+1,j) >>
   pure (BeginShare (i,j) >>
-      Put (i, j+1) >>
+      Put (i,j+1) >>
       p >>= fun x =>
       Put (i+1,j) >>
       EndShare (i,j) >>
