dbc7579ffaa98bb5ce617a36694e96cd336e61d9
Daniel Teut <danielteut@t-online.de>
2020-07-14 01:24:41 +0200

    Remove `runPipeline` from Agda `convertModule`

--- src/lib/FreeC/Backend/Agda/Converter/Module.hs
+++ src/lib/FreeC/Backend/Agda/Converter/Module.hs
@@ -2,7 +2,6 @@
 
 module FreeC.Backend.Agda.Converter.Module where
 
-import           Control.Monad                  ( (>=>) )
 import           Control.Monad.Extra            ( concatMapM )
 
 import           Data.List.Extra                ( splitOn )
@@ -21,22 +20,17 @@ import           FreeC.IR.DependencyGraph       ( groupTypeDecls
 import           FreeC.IR.Pragma
 import qualified FreeC.IR.Syntax               as IR
 import           FreeC.Monad.Converter
-import           FreeC.Pipeline
 
--- | Converts a Haskell module to an Agda declaration.
+-- | Converts an IR module to an Agda declaration.
 convertModule :: IR.Module -> Converter Agda.Declaration
-convertModule = moduleEnv . (runPipeline >=> convertModule')
-
--- | Like 'convertModule'' but does not apply any compiler passes beforehand.
-convertModule' :: IR.Module -> Converter Agda.Declaration
-convertModule' modul@(IR.Module _ name _ typeDecls _ _ funcDecls) = do
+convertModule modul@(IR.Module _ name _ typeDecls _ _ funcDecls) = do
   mapM_ (addDecArgPragma (IR.modFuncDecls modul)) (IR.modPragmas modul)
   Agda.moduleDecl (convertModName name) <$> getAp (typeDecls' <> funcDecls')
  where
   typeDecls' = Ap $ concatMapM convertTypeDecls $ groupTypeDecls typeDecls
   funcDecls' = Ap $ concatMapM convertFuncDecls $ groupFuncDecls funcDecls
 
--- | Converts a Haskell module name to an Agda module name
+-- | Converts an IR module name to an Agda module name
 convertModName :: IR.ModName -> Agda.QName
 convertModName name = Agda.qname (init parts) (last parts)
   where parts = Agda.name <$> splitOn "." name
