f7bb81ddfee8603a9fcea1fac2fafd7012a5b614
Marvin Lira <stu204529@mail.uni-kiel.de>
2020-07-14 20:30:34 +0200

    Improve `RazorProofsPureCodes.v` in a new file #82

---
+++ example/CaseStudyHuttonsRazor/RazorProofsPureCodesPretty.v
@@ -0,0 +1,159 @@
+From Base Require Import Free Free.Instance.Maybe Free.Instance.Error Prelude Test.QuickCheck.
+From Razor.Extra Require Import ExprInd FreeDB Tactic Pureness.
+From Generated Require Import Razor.
+From Proofs Require Import AppendAssocProofs.
+
+Require Import Coq.Logic.FunctionalExtensionality.
+Require Import Coq.Program.Equality.
+
+(* This property states, that the given Partial instance represents every [undefined] as an impure value. *)
+Definition UndefinedIsImpure {Shape : Type} {Pos : Shape -> Type} (Part : Partial Shape Pos): Prop :=
+  forall (A : Type),
+  exists (s : Shape) (pf : (Pos s) -> Free Shape Pos A),
+      undefined = impure s pf.
+
+(* The property holds for the [Maybe] monad and the [Error] monad. *)
+Example undefinedIsImpureMaybe : UndefinedIsImpure Maybe.Partial.
+Proof.
+  intro A.
+  simpl. unfold Nothing. exists tt.
+  exists (fun p : Maybe.Pos tt => match p return (Free unit Maybe.Pos A) with end).
+  reflexivity.
+Qed.
+
+Example undefinedIsImpureError : UndefinedIsImpure Error.Partial.
+Proof.
+  intro A.
+  simpl. unfold ThrowError. exists "undefined"%string.
+  exists (fun p : Error.Pos "undefined"%string => match p return (Free string Error.Pos A) with end).
+  reflexivity.
+Qed.
+
+(* This property states, that the given Partial instance has no positions in an impure [undefined]. *)
+Definition UndefinedHasNoPositions {Shape : Type} {Pos : Shape -> Type} (Partial : Partial Shape Pos): Prop :=
+  forall (A : Type)
+         (s : Shape)
+         (pf : (Pos s) -> Free Shape Pos A),
+  undefined = impure s pf ->
+  forall (p : Pos s),
+      False.
+
+Section Proofs_PureCodes.
+
+  Variable Shape   : Type.
+  Variable Pos     : Shape -> Type.
+  Variable Part : Partial Shape Pos.
+
+  (* If the code is pure and the first operation is pure if there is any, the
+     effect of an impure stack will transfer to the result of an exec call with
+     that code and that stack. *)
+  Lemma exec_strict_on_stack_arg_Nil :
+    UndefinedIsImpure Part ->
+    UndefinedHasNoPositions Part ->
+        exec Part Nil undefined
+        = undefined.
+  Proof.
+    intros HUndefined1 HUndefined2.
+    specialize (HUndefined1 (Stack Shape Pos)).
+    destruct HUndefined1 as [ s [ pf HUndefined1 ] ].
+    rewrite HUndefined1.
+    autodef.
+    f_equal.
+    extensionality p.
+    specialize (HUndefined2 (Stack Shape Pos) s pf HUndefined1 p).
+    destruct HUndefined2.
+  Qed.
+
+  Lemma exec_strict_on_stack_arg_Op :
+    UndefinedIsImpure Part ->
+    UndefinedHasNoPositions Part ->
+    forall (op : Op Shape Pos)
+           (fcode : Free Shape Pos (Code Shape Pos)),
+        exec Part (Cons (pure op) fcode) undefined
+        = undefined.
+  Proof.
+    intros HUndefined1 HUndefined2 op fcode.
+    specialize (HUndefined1 (Stack Shape Pos)).
+    destruct HUndefined1 as [ s [ pf HUndefined1 ] ].
+    rewrite HUndefined1.
+    autodef.
+    f_equal.
+    extensionality p.
+    specialize (HUndefined2 (Stack Shape Pos) s pf HUndefined1 p).
+    destruct HUndefined2.
+  Qed.
+
+  (* If [UndefinedIsImpure] and [UndefinedHasNoPositions] hold and we are given
+     two recursively pure pieces of code and a stack, we know that it doesn't
+     matter wether we concatenate those pieces of code and run them on the
+     stack, or run the first piece of code on the stack and run the second
+     piece of code on the resulting stack afterwards. *)
+  Lemma exec_append :
+    UndefinedIsImpure Part ->
+    UndefinedHasNoPositions Part ->
+    forall (fcode1 : Free Shape Pos (Code Shape Pos)),
+    RecPureCode fcode1 ->
+    forall (fcode2 : Free Shape Pos (Code Shape Pos)),
+    RecPureCode fcode2 ->
+    forall (fstack : Free Shape Pos (Stack Shape Pos)),
+        exec Part (append fcode1 fcode2) fstack
+        = exec Part fcode2 (exec Part fcode1 fstack).
+  Proof with pretty; try reflexivity.
+    intros HUndefined1 HUndefined2 fcode1 HPure1 fcode2 HPure2.
+    (* As we now that both pieces of code are recursively pure we, we can
+       immediately destruct the monadic layer.*)
+    destruct fcode1 as [ code1 | ]; [ | dependent destruction HPure1 ].
+    destruct fcode2 as [ code2 | ]; [ | dependent destruction HPure2 ].
+    induction code1 as [ | [ [ fn | ] | ] fcode1' IHfcode1' ] using List_Ind; [ | | | dependent destruction HPure1 ]; pretty.
+    - (* code1 = [] *)
+      (* This case is trivial. *)
+      intro fstack.
+      autodef.
+      f_equal.
+      induction fstack as [ stack | sStack pfStack IHpfStack ] using Free_Ind; autodef; autoIH.
+    - (* code1 = (PUSH fn : fcode1') *)
+      (* This case is also easy. *)
+      intro fstack.
+      dependent destruction HPure1.
+      destruct fcode1' as [ code1' | ]; [ | dependent destruction HPure1 ].
+      simplify IHfcode1' as IH.
+      (* We can proof the goal per induction over the stack. *)
+      induction fstack as [ stack | sStack pfStack IHpfStack ] using Free_Ind; autodef; try apply (IH HPure1).
+      (* In the impure case we need to destruct the second piece of code to be able to simplify
+         the goal before applying the induction hypothesis in the pure cases. *)
+      destruct code2 as [ | [ op | ] fcode2' ]; autodef; try autoIH; dependent destruction HPure2.
+    - (* code1 = (ADD : fcode1') *)
+      (* In this case the [exec] function might return [undefined] and we need
+         our two assumptions for an [undefined] [Stack]. *)
+      intro fstack.
+      dependent destruction HPure1.
+      destruct fcode1' as [ code1' | ]; [ | dependent destruction HPure1 ].
+      simplify IHfcode1' as IH.
+      (* As the addition reads two values from the stack, we need to destruct the stack. *)
+      induction fstack as [ [ | fv1 fstack1 ] | sStack pfStack IHpfStack ] using Free_Ind...
+      + (* fstack = [] *)
+        (* On both sides an [undefined] is returned. *)
+        autodef.
+        destruct code2 as [ | [ op | ] fcode2' ]; [ | | dependent destruction HPure2 ]...
+        { rewrite (exec_strict_on_stack_arg_Nil HUndefined1 HUndefined2)... }
+        { rewrite (exec_strict_on_stack_arg_Op HUndefined1 HUndefined2)... }
+      + induction fstack1 as [ [ | fv2 fstack2 ] | sStack1 pfStack1 IHpfStack1 ] using Free_Ind...
+        * (* fstack = (fv1 : fstack1) *)
+          (* On both sides an [undefined] is returned. *)
+          autodef.
+          destruct code2 as [ | [ op | ] fcode2' ]; [ | | dependent destruction HPure2 ]...
+          { rewrite (exec_strict_on_stack_arg_Nil HUndefined1 HUndefined2)... }
+          { rewrite (exec_strict_on_stack_arg_Op HUndefined1 HUndefined2)... }
+        * (* fstack = (fv1 : fv2 : fstack2) *)
+          (* The function definition can be applied and we can use the induction hypothesis. *)
+          autodef.
+          apply (IH HPure1).
+        * (* fstack = (fv1 : impure sStack1 pfStack1) *)
+          autodef.
+          destruct code2 as [ | [ op | ] fcode2' ]; autodef; try autoIH; dependent destruction HPure2.
+      + (* fstack = impure sStack2 pfStack2 *)
+        autodef.
+        destruct code2 as [ | [ op | ] fcode2' ]; autodef; try autoIH; dependent destruction HPure2.
+  Qed.
+
+End Proofs_PureCodes.
