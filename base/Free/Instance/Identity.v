382ebd5389c6e76b912a16ef67fa22ca918766d7
Maja <stu126115@mail.uni-kiel.de>
2020-07-15 09:37:15 +0200

    Start putting handlers into effect modules #116

--- base/Free/Instance/Identity.v
+++ base/Free/Instance/Identity.v
@@ -1,6 +1,7 @@
 (** * Definition of the identity monad in terms of the free monad. *)
 
 From Base Require Import Free.
+From Base Require Import Free.Instance.Comb.
 From Base Require Import Free.Util.Void.
 
 Module Identity.
@@ -19,10 +20,19 @@ Module Identity.
     : Free Shape' Pos' A := pure x.
   End Monad.
 
+  (* Handler for an effect-free program. *)
+  Module Import Handler.
+    Definition run {A : Type} (fz : Free Identity.Shape Identity.Pos A) : A 
+      := match fz with 
+         | pure x => x
+         | impure s _ => match s with end
+         end.
+  End Handler.
   (* There is no partial instance for the identity monad. *)
 End Identity.
 
 (* The type and smart constructor should be visible to other modules
    but to use the shape or position function the identifier must be
    fully qualified, i.e. [Identity.Shape]. *)
+Export Identity.Handler.
 Export Identity.Monad.
--- base/Free/Instance/ND.v
+++ base/Free/Instance/ND.v
@@ -1,6 +1,8 @@
 (** * Definition of the non-determinism effect in terms of the free monad. *)
 
 From Base Require Import Free.
+From Base Require Import Free.Instance.Comb.
+From Base Require Import Free.Util.Search.
 From Base Require Import Free.Util.Void.
 
 Module ND.
@@ -37,8 +39,31 @@ Module ND.
     Notation "x ? y" := (Choice None x y) (at level 80).
  End Monad.
 
+  (* Handlers for non-determinism and call-time choice. *)
+  Module Import Handler.
+    Definition SChoice (Shape' : Type) := Comb.Shape Shape Shape'.
+    Definition PChoice {Shape' : Type} (Pos' : Shape' -> Type) 
+    := Comb.Pos Pos Pos'.
+    (* Non-determinism effect handler. *)
+    Fixpoint runChoice {A : Type} 
+                       {Shape' : Type} 
+                       {Pos' : Shape' -> Type} 
+                       (fc : Free (SChoice Shape') (PChoice Pos') A) 
+     : Free Shape' Pos' (Tree A) 
+    := match fc with
+       | pure x => pure (Leaf x)
+       | impure (inl ND.sfail) _ => pure (Empty A)
+       | impure (inl (ND.schoice mid)) pf =>
+         runChoice (pf true) >>= fun l =>
+         runChoice (pf false) >>= fun r =>
+         pure (Branch mid l r)
+       | impure (inr s) pf =>
+         impure s (fun p => runChoice (pf p))
+       end.
+  End Handler.
 
-  (* Partial instance for the non-determinism monad. *)
+
+  (* Partial instance for the non-determinism effect. *)
   Instance Partial : Partial Shape Pos := {
       undefined := fun {A : Type}                => Fail;
       error     := fun {A : Type} (msg : string) => Fail
