f173ea1d843c6841145ff00f200b0027595c4ef1
Maja <stu126115@mail.uni-kiel.de>
2020-07-16 11:40:56 +0200

    Refactor Search module #116

--- base/Free/Util/Search.v
+++ base/Free/Util/Search.v
@@ -1,12 +1,14 @@
-(** Definition of choice trees and the depth-first search algorithm *)
+(** Definition of choice trees and the depth-first search algorithm, 
+    as well as lists where entries have IDs and a function that filters
+    out entries with duplicate IDs. *)
 
+From Base Require Import Free.Util.Sharing.
 Require Import EqNat.
 Require Import Coq.Strings.String.
 
 Set Implicit Arguments.
 
-Definition ID : Type := nat * nat * nat.
-
+(* Definition of a choice tree *)
 Inductive Tree A :=
 | Empty  : Tree A
 | Leaf   : A -> Tree A
@@ -14,39 +16,41 @@ Inductive Tree A :=
 
 Inductive Decision := L | R.
 
-Definition beq_id (id1 id2 : ID) : bool :=
+(* ID equality *)
+Definition beqId (id1 id2 : ID) : bool :=
   match id1,id2 with
   | (n11, n12, n13), (n21, n22, n23) => andb (andb (beq_nat n11 n21) (beq_nat n12 n22))
                                             (beq_nat n13 n23)
   end.
 
 (** Maps are defined as functions *)
-Definition total_map (K V : Type) := K -> V.
+Definition totalMap (K V : Type) := K -> V.
 
-Definition partial_map (K V : Type) := total_map K (option V).
+Definition partialMap (K V : Type) := totalMap K (option V).
 
-Definition tmap_empty {K V : Type} (v : V) : total_map K V := (fun _ => v).
+Definition tmapEmpty {K V : Type} (v : V) : totalMap K V := (fun _ => v).
 
-Definition emptymap {K V :Type} : partial_map K V := tmap_empty None.
+Definition emptymap {K V :Type} : partialMap K V := tmapEmpty None.
 
-Definition t_update {K V : Type} (beq : K -> K -> bool) (m : total_map K V) (k : K) (v : V) :=
+Definition tUpdate {K V : Type} (beq : K -> K -> bool) (m : totalMap K V) (k : K) (v : V) :=
   fun k' => if beq k k' then v else m k'.
 
-Definition update {K V : Type} (beq : K -> K -> bool) (m : partial_map K V) (k : K) (v : V) :=
-  t_update beq m k (Some v).
+Definition update {K V : Type} (beq : K -> K -> bool) (m : partialMap K V) (k : K) (v : V) :=
+  tUpdate beq m k (Some v).
 
-Definition Memo := partial_map ID Decision.
+Definition Memo := partialMap ID Decision.
 
-Definition List_Memo := partial_map ID unit.
+Definition ListMemo := partialMap ID unit.
 
+(* Collect values of a choice tree while respecting shared decisions. *)
 Fixpoint dfs A (m : Memo) (t : Tree A) : list A :=
   match t with
   | Empty _ => Datatypes.nil
   | Leaf x => Datatypes.cons x Datatypes.nil
   | Branch None l r => app (dfs m l) (dfs m r)
   | Branch (Some id) l r => match m id with
-                           | None => app (dfs (update beq_id m id L) l)
-                                        (dfs (update beq_id m id R) r)
+                           | None => app (dfs (update beqId m id L) l)
+                                        (dfs (update beqId m id R) r)
                            | Some L => dfs m l
                            | Some R => dfs m r
                            end
@@ -54,7 +58,8 @@ Fixpoint dfs A (m : Memo) (t : Tree A) : list A :=
 
 Definition collectVals A := @dfs A emptymap.
 
-Fixpoint compute_log (m : List_Memo) (xs : list (option ID * string)) : list string :=
+(* Filter out duplicate messages from a list of messages with sharing IDs. *)
+Fixpoint compute_log (m : ListMemo) (xs : list (option ID * string)) : list string :=
   match xs with
   | nil                    => nil
   | cons (None, msg)    ys => cons msg (compute_log m ys)
