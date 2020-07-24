851d0783be8d59c6c924dba504e648426a3e78b9
jhoefer <jonas.hoefer99@gmail.com>
2020-07-24 17:36:48 +0200

    Remove duplicate coq files

--- base/coq/Free/Partial.v
+++
@@ -1,9 +0,0 @@
-From Base Require Import Free.Monad.
-
-Require Export Coq.Strings.String.
-
-Class Partial (Shape : Type) (Pos : Shape -> Type) :=
-  {
-    undefined : forall {A : Type}, Free Shape Pos A;
-    error     : forall {A : Type}, string -> Free Shape Pos A
-  }.
