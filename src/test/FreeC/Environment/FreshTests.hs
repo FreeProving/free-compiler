e06fbf5cb458e1e05c8254fee49d99d0867dc7e7
Marvin Lira <stu204529@mail.uni-kiel.de>
2020-07-27 22:06:21 +0200

    Adjust formatting #154

--- src/test/FreeC/Environment/FreshTests.hs
+++ src/test/FreeC/Environment/FreshTests.hs
@@ -18,9 +18,9 @@ testFreshIdentifiers = describe "FreeC.Environment.Fresh" $ do
       x1 <- freshCoqIdent "x"
       return ((x0, x1) `shouldBe` ("x", "x0"))
     it "avoids collision with user defined identifiers" $ shouldSucceedWith $ do
-      "x"   <- defineTestVar "x"
-      x1    <- freshCoqIdent "x" -- 'x@0' is renamed to 'x0'
-      x2    <- freshCoqIdent "x" -- 'x@1' is renamed to 'x1'
+      "x" <- defineTestVar "x"
+      x1  <- freshCoqIdent "x" -- 'x@0' is renamed to 'x0'
+      x2  <- freshCoqIdent "x" -- 'x@1' is renamed to 'x1'
       return ((x1, x2) `shouldBe` ("x0", "x1"))
     it "is affected by local environments" $ shouldSucceedWith $ do
       x1 <- localEnv $ do
