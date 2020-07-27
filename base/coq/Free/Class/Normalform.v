bdcf148e42deeea92ff5cbba65ebefe2252dc4f3
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-07-27 13:28:48 +0200

    Add Normalform typeclass #148
    This commit adds a typeclass Normalform to move
    effects from the components of an expression to its
    root.
    
    The implementation is a variation of the one
    from Niels' thesis: https://github.com/nbun/mathesis/blob/master/Coq/src/Classes.v

---
+++ base/coq/Free/Class/Normalform.v
@@ -0,0 +1,18 @@
+(** Type class for the normalization of data types with effectful components.
+    Moves effects from components to the root of the expression. *)
+From Base Require Import Free.Monad.
+
+Class Normalform (Shape : Type) (Pos : Shape -> Type)
+                 (A B : Type) :=
+  {
+    (** The function is split into two parts due to termination check errors
+        for recrusive data types. *)
+    nf  : Free Shape Pos A -> Free Shape Pos B;
+    nf' : A -> Free Shape Pos B;
+    (** Property for moving nf into position functions *)
+    nf_impure: forall s (pf : _ -> Free Shape Pos A),
+        nf (impure s pf) = impure s (fun p => nf (pf p));
+    (** Property for unfolding nf on pure values *)
+    nf_pure : forall (x : A),
+        nf (pure x) = nf' x
+  }.
