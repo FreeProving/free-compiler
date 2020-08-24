1faa4cc10f21b7272cbd30013172e0b520c0595d
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-08-24 19:14:40 +0200

    Add ShareableArgs constraint to the type of share #149
    The function share defined by the class Shareable
    is now only defined for those types A that have an
    associated ShareableArgs instance.
    Otherwise, it is not possible to define the
    call-by-need instance of Shareable, which will
    make use of shareArgs.
    
    Since ShareableArgs instances are generally defined
    using cbneed, the class Shareable now requires that instances be compatible with that function, i.e.
    that their associated effect stack includes the
    Share effect.
    This constraint is unfortunate since the call-by-name
    instance doesn't technically need it as it never
    makes use of shareArgs, but at present, it is
    necessary.

--- base/coq/Free/Class/Shareable.v
+++ base/coq/Free/Class/Shareable.v
@@ -1,6 +1,12 @@
-From Base Require Import Free.Monad.
+From Base Require Export Free.Instance.Share.
 
-Class Shareable (Shape : Type) (Pos : Shape -> Type) :=
+From Base Require Import Free.Monad.
+From Base Require Import Free.Class.Injectable.
+From Base Require Import Free.Class.ShareableArgs.
+
+Class Shareable (Shape : Type) (Pos : Shape -> Type) 
+                `{Injectable Share.Shape Share.Pos Shape Pos} :=
   {
-    share : forall {A : Type}, Free Shape Pos A -> Free Shape Pos (Free Shape Pos A)
+    share : forall {A : Type} `{S : ShareableArgs Shape Pos A}, 
+      Free Shape Pos A -> Free Shape Pos (Free Shape Pos A)
   }.
