3febd99cc38e5bffb9edadd4d9ccc1935722c9de
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-08-24 18:49:11 +0200

    Export ShareableArgs in Class.v #149

--- base/coq/Free/Class.v
+++ base/coq/Free/Class.v
@@ -2,4 +2,5 @@ From Base Require Export Free.Class.Injectable.
 From Base Require Export Free.Class.Normalform.
 From Base Require Export Free.Class.Partial.
 From Base Require Export Free.Class.Shareable.
+From Base Require Export Free.Class.ShareableArgs.
 From Base Require Export Free.Class.Traceable.
