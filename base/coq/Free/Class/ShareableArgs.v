8b71b6c70cff7eb2999d29a1411503b89049a9da
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-08-30 08:34:25 +0200

    Add ShareableArgs instance for functions #149

--- base/coq/Free/Class/ShareableArgs.v
+++ base/coq/Free/Class/ShareableArgs.v
@@ -5,3 +5,9 @@ Class ShareableArgs (Shape : Type) (Pos : Shape -> Type)
   {
     shareArgs : A -> Free Shape Pos A
   }.
+
+(* ShareableArgs instance for functions. 
+   Effects inside of functions are not shared. *)
+Instance ShareableArgsFunc (Shape : Type) (Pos : Shape -> Type) (A B : Type)
+ : ShareableArgs Shape Pos (A -> B) :=
+  { shareArgs := pure }.
