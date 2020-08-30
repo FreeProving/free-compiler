bd3e3dde8ee2dc17c3fe1a65cc4c17a74278bd7c
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-08-30 13:26:56 +0200

    Add empty sharing handler #177
    This commits adds a sharing handler to Share for effects that do
    not require sharing (as of the time of this commit, that is
    every effect except for Trace and ND).
    
    Previously, to handle a program that contains sharing, but no
    relevant effect, one of the relevant effects would unnecessarily
    be added to the effect stack.
    With this handler, this is no longer necessary.
    
    However, this handler should NOT be used to handle relevant effects,
    as no sharing identifiers will actually be assigned.

--- base/coq/Free/Instance/Share.v
+++ base/coq/Free/Instance/Share.v
@@ -1,6 +1,7 @@
 (** * Definition of the sharing effect in terms of the free monad. *)
 
 From Base Require Import Free.Class.Injectable.
+From Base Require Import Free.Instance.Comb.
 From Base Require Import Free.Monad.
 
 Module Share.
@@ -42,6 +43,55 @@ Module Share.
     : Free Shape' Pos' unit :=
     impure (injS (sesharing n)) (fun _ => pure tt).
   End Monad.
+
+  (* Sharing handler for effects that do not require sharing. *)
+  Module Import Handler.
+
+    Fixpoint runEmptySharing {A : Type} 
+                          {Shape' : Type} 
+                          {Pos' : Shape' -> Type}
+                          (n : nat * nat)
+                          (fs : Free (Comb.Shape Shape Shape') (Comb.Pos Pos Pos') A) 
+     : Free Shape' Pos' A 
+    := let fix runInnerScope (next : nat)
+                           (state : nat * nat)
+                           (scope : nat * nat) 
+                           (scopes : list (nat * nat)) 
+                           (fs : Free (Comb.Shape Shape Shape') (Comb.Pos Pos Pos') A)
+     : Free Shape' Pos' A
+       := match fs with (* inside scope handler *)
+          | pure x => pure x
+          | impure (inl (Share.sbsharing n')) pf =>  (* open nested scope *)
+             runInnerScope 1 state n' (cons n' scopes) (pf tt)
+          | impure (inl (Share.sesharing n')) pf => 
+             match scopes with
+              (* leave nested scope *)
+            | cons _ (cons j js) as ks => runInnerScope next state j ks (pf tt)
+              (* leave outermost scope *)
+            | _                        => runEmptySharing state (pf tt)
+            end
+          | impure (inl Share.sget) pf => 
+             runInnerScope next state scope scopes (pf state) (* get state *)
+          | impure (inl (Share.sput n')) pf => (* set new state *)
+             runInnerScope next n' scope scopes (pf tt)
+          | impure (inr s) pf => (* other effect *)
+             impure s (fun p => runInnerScope next state scope scopes (pf p))
+          end
+       in match fs with (* outside scope handler *)
+          | pure x => pure x
+          | impure (inl (Share.sbsharing n'))  pf => (* enter sharing scope *)
+            runInnerScope 1 n n' (cons n' nil) (pf tt)
+          | impure (inl (Share.sesharing n'))  pf => (* error *)
+            (runEmptySharing n (pf tt))
+          | impure (inl Share.sget) pf => (* get state *)
+            runEmptySharing n (pf n)
+          | impure (inl (Share.sput n')) pf => (* set new state *)
+            (runEmptySharing n' (pf tt))
+          | impure (inr s) pf => impure s (fun p => runEmptySharing n (pf p)) (* other effect *)
+          end.
+
+  End Handler.
 End Share.
 
+Export Share.Handler.
 Export Share.Monad.
