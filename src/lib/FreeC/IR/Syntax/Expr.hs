0c04cb4ef6c02936c341f7dadb5be3ea0e281500
Justin Andresen <jan.dresen.95@gmail.com>
2020-09-11 22:36:54 +0200

    Add type class for nodes with declaration name #158

--- src/lib/FreeC/IR/Syntax/Expr.hs
+++ src/lib/FreeC/IR/Syntax/Expr.hs
@@ -310,6 +310,11 @@ data VarPat = VarPat { varPatSrcSpan  :: SrcSpan
                      }
  deriving ( Eq, Show )
 
+-- | Instance to get the name of a @let@-binding..
+instance HasDeclIdent VarPat where
+  declIdent varPat = DeclIdent (varPatSrcSpan varPat)
+    (UnQual (Ident (varPatIdent varPat)))
+
 -- | Gets the name of the given variable pattern.
 varPatName :: VarPat -> Name
 varPatName = Ident . varPatIdent
@@ -340,10 +345,14 @@ instance Pretty VarPat where
 -------------------------------------------------------------------------------
 -- @let@ Bindings                                                            --
 -------------------------------------------------------------------------------
--- | A binding of a variable to an expression inside of a let clause
+-- | A binding of a variable to an expression inside of a @let@-expression.
 data Bind
   = Bind { bindSrcSpan :: SrcSpan, bindVarPat :: VarPat, bindExpr :: Expr }
  deriving ( Eq, Show )
+
+-- | Instance to get the name of a @let@-binding..
+instance HasDeclIdent Bind where
+  declIdent = declIdent . bindVarPat
 
 -- | Pretty instance for @let@ expression binds.
 instance Pretty Bind where
