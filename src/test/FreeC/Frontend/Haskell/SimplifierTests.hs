4ad8bd2a1a7db0d70248bf4e4c1d9366de04d6ef
Justin Andresen <jan.dresen.95@gmail.com>
2020-09-14 15:50:09 +0200

    Format code #74

--- src/test/FreeC/Frontend/Haskell/SimplifierTests.hs
+++ src/test/FreeC/Frontend/Haskell/SimplifierTests.hs
@@ -1,24 +1,24 @@
 -- | This module contains tests for "FreeC.Frontend.Haskell.Simplifier".
-module FreeC.Frontend.Haskell.SimplifierTests (testSimplifier) where
+module FreeC.Frontend.Haskell.SimplifierTests ( testSimplifier ) where
 
-import Control.Monad ( (>=>) )
-import Test.Hspec
+import           Control.Monad                     ( (>=>) )
+import           Test.Hspec
 
-import qualified FreeC.IR.Syntax as IR
 import           FreeC.Frontend.Haskell.Parser
 import           FreeC.Frontend.Haskell.Simplifier
+import           FreeC.IR.SrcSpan
+import qualified FreeC.IR.Syntax                   as IR
 import           FreeC.Monad.Class.Testable
 import           FreeC.Test.Expectations
 import           FreeC.Test.Parser
-import           FreeC.IR.SrcSpan
 
 -------------------------------------------------------------------------------
 -- Utility functions                                                         --
 -------------------------------------------------------------------------------
-
 -- | Parses a Haskell expression and converts it to IR.
 parseAndSimplifyExpr :: String -> Simplifier IR.Expr
-parseAndSimplifyExpr = (parseHaskell . mkSrcFile "<test-input>") >=> simplifyExpr
+parseAndSimplifyExpr = (parseHaskell . mkSrcFile "<test-input>")
+  >=> simplifyExpr
 
 -- | Parses the given Haskell and IR expressions, converts the Haskell
 --   expression to IR .
@@ -31,7 +31,6 @@ shouldSimplifyExpr input expectedOutput = do
 -------------------------------------------------------------------------------
 -- Tests                                                                     --
 -------------------------------------------------------------------------------
-
 -- | Test group for "FreeC.Frontend.Haskell.Simplifier" tests.
 testSimplifier :: Spec
 testSimplifier = describe "FreeC.Frontend.Haskell.Simplifier" $ do
@@ -40,5 +39,7 @@ testSimplifier = describe "FreeC.Frontend.Haskell.Simplifier" $ do
 -- | Test group for 'simplifyExpr' tests.
 testSimplifyExpr :: Spec
 testSimplifyExpr = context "simplifyExpr" $ do
-  it "simplifies single variable pattern case expression to lambda abstractions" $ shouldSucceedWith $ do
-    "case e of { x -> e' }" `shouldSimplifyExpr` "(\\x -> e') e"
+  it "simplifies single variable pattern case expression to lambda abstractions"
+    $ shouldSucceedWith
+    $ do
+      "case e of { x -> e' }" `shouldSimplifyExpr` "(\\x -> e') e"
