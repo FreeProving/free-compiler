bb296e3413cb95738de700eb1a1caa1c9ede7447
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-09-15 23:00:02 +0200

    Add a few more example type to test generated instances #150 #151

--- example/Proofs/TypeclassInstances.hs
+++ example/Proofs/TypeclassInstances.hs
@@ -58,3 +58,24 @@ doubleDisjunctionHeadRoot t = doubleDisjunction t headRoot
 -- doubleDisjunction specialized for Map
 doubleDisjunctionMap :: Map Bool Bool -> Bool
 doubleDisjunctionMap m = doubleDisjunction m firstMapEntry
+
+-- Additional data types to check that the generated
+-- instances are valid Coq code
+--Types with potential name conflict
+data T a = TCons a
+
+data T_ = T_Cons
+
+-- Type with nested recursion and type variable instantiation
+data Rose a = Rose (Rose Integer, Rose a)
+
+-- Mutually recursive types with nested recursion and type variable
+-- instantiation
+data A a = ConsA [B Bool] | AVal a
+
+data B a = ConsB [A Bool] | BVal a
+
+-- Indirect recursion hidden in a type synonym
+type IntGatherings = MyList (Gathering Integer)
+
+data Gathering a = Many IntGatherings | Single a
