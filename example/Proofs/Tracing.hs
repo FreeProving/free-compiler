4bdc22e00a09d36f3003740eef7edeee15c1bc30
Justin Andresen <jan.dresen.95@gmail.com>
2020-09-17 21:08:07 +0200

    Format example code #134

--- example/Proofs/Tracing.hs
+++ example/Proofs/Tracing.hs
@@ -1,13 +1,13 @@
 -- | This example demonstrates how to prove properties that involve the
 --   sharing and tracing effects.
-
 module Proofs.Tracing where
 
-import Debug.Trace
-import Test.QuickCheck
+import           Debug.Trace
+import           Test.QuickCheck
 
 foo :: Integer
-foo = let x = trace "x" 1 in trace "foo" (x + x)
+foo = let x = trace "x" 1
+      in trace "foo" (x + x)
 
 foo' :: Integer
 foo' = trace "foo" (trace "x" 2)
