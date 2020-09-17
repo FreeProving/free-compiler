2e367301c6c287477e54148c49d6111e946e47c3
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-09-17 18:43:28 +0200

    Add Handler class with two functions #202
    The Handler class has been added again, but with only one
    type parameter.
    Instead of the result type parameter B, the Handler class now
    contains a function transformType : Type -> Type that describes
    how an input type changes when it is handled.
    The result type of the handler is now determined by applying
    transformType to the result type of normalizing the input type A.

---
+++ base/coq/Free/Class/Handler.v
@@ -0,0 +1,18 @@
+From Base Require Import Free.Class.Normalform.
+From Base Require Import Free.Monad.
+
+(** A class representing handlers for whole effect stacks. *)
+Class Handler (Shape : Type) (Pos : Shape -> Type) (A : Type)
+              `{Normalform Shape Pos A} := {
+  (* This function defines how a type is transformed by the handling process.
+     Does not usually have to be implemented when defining an instance, as
+     it can be inferred from the type of handle. *)
+  transformType : Type -> Type;
+  (* This function defines how the given effect stack is handled.
+     Its return type is the return type of the normalization as
+     transformed by the handler.
+     For example, for a handler for the effect stack containing only the Maybe
+     effect, a Free Shape Pos (Bool Shape Pos) is turned into an
+     option (Bool Identity.Shape Identity.Pos). *)
+  handle : Free Shape Pos A -> transformType nType
+}.
