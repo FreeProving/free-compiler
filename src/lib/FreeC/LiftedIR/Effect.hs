410ca2e9cfff7f80917941d571d2cca4ae4f0c0e
Justin Andresen <jan.dresen.95@gmail.com>
2020-09-17 15:30:56 +0200

    Generalize `EffectAnalysisPass` #134

--- src/lib/FreeC/LiftedIR/Effect.hs
+++ src/lib/FreeC/LiftedIR/Effect.hs
@@ -4,6 +4,7 @@ module FreeC.LiftedIR.Effect where
 -- | An effect constraint.
 --
 --   These effects corresponds to type classes constraining the container used
---   by the @Free@ monad.
+--   by the @Free@ monad. The order of the constructors in this data type
+--   determines the order of constraints in the generated function declarations.
 data Effect = Partiality | Sharing | Tracing
- deriving ( Show, Eq )
+ deriving ( Show, Eq, Ord )
