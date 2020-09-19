e00ae88b1ab7ef3c7fb7afb47f0edae5e54df8dd
Justin Andresen <jan.dresen.95@gmail.com>
2020-09-19 12:20:38 +0200

    Fix typo #134

--- example/Proofs/Tracing.v
+++ example/Proofs/Tracing.v
@@ -4,5 +4,5 @@ From Generated Require Import Proofs.Tracing.
 Theorem trace_is_shared : quickCheck prop_trace_is_shared.
 Proof.
   intros Shape Pos I S T. simpl.
-  (* TODO Complete prove once [quickCheck] can is parameterized over handler. *)
+  (* TODO Complete prove once [quickCheck] is parameterized over handler. *)
 Admitted.
