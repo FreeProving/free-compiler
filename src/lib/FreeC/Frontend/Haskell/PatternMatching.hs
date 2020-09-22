6f6cbc3ac366723fc00d971b200f5ac1189e6322
Justin Andresen <jan.dresen.95@gmail.com>
2020-09-22 16:31:06 +0200

    Use effects to run pattern matching compiler #210
    Some effects still have to be initialized properly.
    Furthermore the HSE frontend of the pattern matching
    compiler is not general enought. It has to be parameterized
    over arbitrary source spans such that we can use or
    source span data type.

--- src/lib/FreeC/Frontend/Haskell/PatternMatching.hs
+++ src/lib/FreeC/Frontend/Haskell/PatternMatching.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
 -- | This module provides an interface to the pattern matching compiler and
 --   case completion library by Malte Clement
 --   <https://git.informatik.uni-kiel.de/stu204333/placc-thesis>.
@@ -5,97 +13,29 @@
 --   <https://github.com/FreeProving/haskell-src-transformations>.
 module FreeC.Frontend.Haskell.PatternMatching ( transformPatternMatching ) where
 
-import           Application
-import           Control.Monad                     ( void )
-import           Data.Map.Strict                   ( Map )
 import qualified Data.Map.Strict                   as Map
-import           Data.Maybe                        ( mapMaybe )
-import qualified Data.Set                          as Set
-import           FreshVars
+import           HST.Frontend.HSE.Config           ( HSE )
+import qualified HST.Application                   as HST
+import qualified HST.Effect.Env                    as HST
+import qualified HST.Effect.Fresh                  as HST
+import qualified HST.Effect.GetOpt                 as HST
+import qualified HST.Effect.InputFile              as HST
+import qualified HST.Effect.Report                 as HST
+import qualified HST.Effect.WithFrontend           as HST
+import qualified HST.Environment                   as HST
+import qualified HST.Frontend.Parser               as HST
+import qualified HST.Frontend.Syntax               as HST
+import qualified HST.Options                       as HST
+import qualified HST.Util.Messages                 as HST
+import qualified HST.Util.Selectors                as HST
 import qualified Language.Haskell.Exts.Syntax      as HSE
+import           Polysemy
+  ( Member, Members, Sem, interpret, runM )
+import           Polysemy.Embed                    ( Embed, embed )
 
-import           FreeC.Environment
-import           FreeC.Environment.Entry
-import           FreeC.Environment.ModuleInterface
-import qualified FreeC.IR.Base.Prelude             as IR.Prelude
 import           FreeC.IR.SrcSpan
-import qualified FreeC.IR.Syntax                   as IR
 import           FreeC.Monad.Converter
-
--- | Constructs the initial state of the pattern matching compiler library.
---
---   The state is initialized with the (unqualified) entries from the prelude
---   module.
-initialState :: Converter PMState
-initialState = do
-  Just preludeIface <- inEnv $ lookupAvailableModule IR.Prelude.modName
-  let entries  = Set.toList (interfaceEntries preludeIface)
-      entries' = mapMaybe makeConsMapEntry entries
-      consMap  = Map.fromListWith (++) entries'
-  return PMState { nextId      = 0
-                 , constrMap   = Map.assocs consMap
-                 , matchedPat  = []
-                 , trivialCC   = False
-                 , opt         = True
-                 , debugOutput = ""
-                 }
-
--- | Converts an entry of the 'Environment' to an entry of the constructor map
---   for the 'initialState'.
-makeConsMapEntry :: EnvEntry -> Maybe (String, [(HSE.QName (), Int, Bool)])
-makeConsMapEntry entry
-  | not (isConEntry entry) = Nothing
-  | otherwise = do
-    typeConIdent <- extractTypeConIdent (entryReturnType entry)
-    return (typeConIdent, [(conQName, arity, isInfix)])
- where
-  -- | Gets the name of the data type from the return type of the constructor.
-  extractTypeConIdent :: IR.Type -> Maybe String
-  extractTypeConIdent (IR.TypeCon _ (IR.UnQual name)) = extractIdent name
-  extractTypeConIdent (IR.TypeCon _ (IR.Qual _ name)) = extractIdent name
-  extractTypeConIdent (IR.TypeApp _ t1 _) = extractTypeConIdent t1
-  extractTypeConIdent _ = Nothing
-
-  extractIdent :: IR.Name -> Maybe String
-  extractIdent (IR.Ident ident) = Just ident
-  extractIdent (IR.Symbol sym)  = Just sym
-
-  -- | The @haskell-src-exts@ name of the constructor.
-  conQName :: HSE.QName ()
-  conQName = convertQName (entryName entry)
-
-  -- | The number of arguments expected by the constructor.
-  arity :: Int
-  arity = entryArity entry
-
-  -- | In Haskell infix operators start with a colon.
-  isInfix :: Bool
-  isInfix = case entryName entry of
-    IR.Qual _ (IR.Symbol (':' : _)) -> True
-    IR.UnQual (IR.Symbol (':' : _)) -> True
-    _ -> False
-
-  -- | Converts a qualifiable IR name to a Haskell name.
-  convertQName :: IR.QName -> HSE.QName ()
-  convertQName qName = case Map.lookup qName specialNames of
-    Just specialName -> HSE.Special () specialName
-    Nothing          -> case qName of
-      (IR.UnQual name) -> convertName name
-      (IR.Qual _ name) -> convertName name
-
-  -- | Converts an unqualified IR name to a Haskell name.
-  convertName :: IR.Name -> HSE.QName ()
-  convertName (IR.Ident ident) = HSE.UnQual () (HSE.Ident () ident)
-  convertName (IR.Symbol sym)  = HSE.UnQual () (HSE.Symbol () sym)
-
-  -- | Maps special IR constructor names to the corresponding Haskell name.
-  specialNames :: Map IR.QName (HSE.SpecialCon ())
-  specialNames = Map.fromList
-    [ (IR.Prelude.unitConName, HSE.UnitCon ())
-    , (IR.Prelude.nilConName, HSE.ListCon ())
-    , (IR.Prelude.consConName, HSE.Cons ())
-    , (IR.Prelude.tupleConName 2, HSE.TupleCon () HSE.Boxed 2)
-    ]
+import           FreeC.Monad.Reporter
 
 -- | Applies the pattern matching transformation, guard elimination and case
 --   completion.
@@ -104,14 +44,90 @@ makeConsMapEntry entry
 --   spans, location information is removed during the transformation.
 transformPatternMatching
   :: HSE.Module SrcSpan -> Converter (HSE.Module SrcSpan)
-transformPatternMatching haskellAst
-  = transformPatternMatching' haskellAst <$> initialState
+transformPatternMatching inputModule = do
+  let inputFilename     = undefined
+      inputFileContents = undefined
+  runM
+    $ HST.runInputFileNoIO (Map.singleton inputFilename inputFileContents)
+    $ reportToReporter
+    $ HST.cancelToReport cancelMessage
+    $ HST.runWithOptions HST.defaultOptions
+    $ HST.runWithFrontendInstances @HSE
+    $ transformPatternMatching' inputModule
+ where
+  cancelMessage :: HST.Message
+  cancelMessage
+    = HST.message HST.Info HST.NoSrcSpan "Pattern matching compilation failed."
 
--- | Removes the source spans of the given Haskell AST and applies the pattern
---   matching compilation.
 transformPatternMatching'
-  :: HSE.Module SrcSpan -> PMState -> HSE.Module SrcSpan
-transformPatternMatching' haskellAst = evalPM $ do
-  let haskellAst' = void haskellAst
-  haskellAst'' <- processModule haskellAst'
-  return (fmap (const NoSrcSpan) haskellAst'')
+  :: ( MonadReporter m
+     , MonadConverter m
+     , Members '[Embed m, HST.GetOpt, HST.Report, HST.WithFrontend HSE] r
+     )
+  => HSE.Module SrcSpan
+  -> Sem r (HSE.Module SrcSpan)
+transformPatternMatching' inputModule = do
+  inputModule' <- HST.transformModule (HST.ParsedModuleHSE (fmap undefined inputModule))
+  env <- initEnv inputModule'
+  HST.runWithEnv env . HST.runFresh (HST.findIdentifiers inputModule') $ do
+    outputModule' <- HST.processModule inputModule'
+    outputModule <- HST.unTransformModule outputModule'
+    return (fmap undefined (HST.getParsedModuleHSE outputModule))
+
+-------------------------------------------------------------------------------
+-- Environment Initialization                                                --
+-------------------------------------------------------------------------------
+initEnv :: (MonadConverter m, Member (Embed m) r)
+        => HST.Module HSE
+        -> Sem r (HST.Environment HSE)
+initEnv _ = return HST.Environment
+  { HST.envCurrentModule   = undefined
+  , HST.envImportedModules = undefined
+  , HST.envOtherEntries    = undefined
+  }
+
+-------------------------------------------------------------------------------
+-- Interpretation for `Report` Effect                                        --
+-------------------------------------------------------------------------------
+-- | Interprets a computation that can report messages in terms of a
+--   'MonadReporter'.
+reportToReporter :: (MonadReporter m, Members '[Embed m, HST.InputFile] r)
+                 => Sem (HST.Report ': r) a
+                 -> Sem r a
+reportToReporter = interpret \case
+  HST.Report msg      -> embed . report =<< convertMessage msg
+  HST.ReportFatal msg -> embed . reportFatal =<< convertMessage msg
+
+-- | Converts a message that has been reported by the pattern matching compiler
+--   to a message that can be reported by a 'MonadReporter'.
+convertMessage :: Member HST.InputFile r => HST.Message -> Sem r Message
+convertMessage (HST.Message severity srcSpan text) = do
+  srcSpan' <- convertMsgSrcSpan srcSpan
+  let severity' = convertSeverity severity
+  return (Message srcSpan' severity' text)
+
+-- | Converts the source span of a message that has been reported by the
+--   pattern matching compiler to our source span data type.
+convertMsgSrcSpan
+  :: Member HST.InputFile r => Maybe HST.MsgSrcSpan -> Sem r SrcSpan
+convertMsgSrcSpan Nothing           = return NoSrcSpan
+convertMsgSrcSpan (Just msgSrcSpan) = do
+  contents <- HST.getInputFile (HST.msgSrcSpanFilePath msgSrcSpan)
+  return SrcSpan
+    { srcSpanFilename    = HST.msgSrcSpanFilePath msgSrcSpan
+    , srcSpanStartLine   = HST.msgSrcSpanStartLine msgSrcSpan
+    , srcSpanStartColumn = HST.msgSrcSpanStartColumn msgSrcSpan
+    , srcSpanEndLine     = HST.msgSrcSpanEndLine msgSrcSpan
+    , srcSpanEndColumn   = HST.msgSrcSpanEndColumn msgSrcSpan
+    , srcSpanCodeLines   = take (HST.msgSrcSpanEndLine msgSrcSpan)
+        (drop (HST.msgSrcSpanStartLine msgSrcSpan) (lines contents))
+    }
+
+-- | Converts the severity of a message that has been reported by the pattern
+--   matching compiler to our message severity data type.
+convertSeverity :: HST.Severity -> Severity
+convertSeverity HST.Internal = Internal
+convertSeverity HST.Error    = Error
+convertSeverity HST.Warning  = Warning
+convertSeverity HST.Info     = Info
+convertSeverity HST.Debug    = Debug
