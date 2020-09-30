ded0ef4b0610bbb71276f4bbbd6a617de6ee0d69
Justin Andresen <jan.dresen.95@gmail.com>
2020-09-30 09:36:39 +0200

    Fix order of effects #203

--- src/lib/FreeC/LiftedIR/Effect.hs
+++ src/lib/FreeC/LiftedIR/Effect.hs
@@ -6,5 +6,8 @@ module FreeC.LiftedIR.Effect where
 --   These effects corresponds to type classes constraining the container used
 --   by the @Free@ monad. The order of the constructors in this data type
 --   determines the order of constraints in the generated function declarations.
-data Effect = Partiality | Sharing | Tracing
+--
+--   The 'Sharing' effect is the first effect by convention. All other effects
+--   are in alphabetical order by default.
+data Effect = Sharing | Partiality | Tracing
  deriving ( Eq, Ord, Read, Show )
