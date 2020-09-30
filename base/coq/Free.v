74f953a4b07659e03f80f0d6d13e994379d68e3f
Maja Reichert <stu126115@mail.uni-kiel.de>
2020-09-30 15:55:38 +0200

    Add Comb to Free exports #76

--- base/coq/Free.v
+++ base/coq/Free.v
@@ -1,6 +1,7 @@
 From Base Require Export Free.Class.
 From Base Require Export Free.ForFree.
 From Base Require Export Free.Induction.
+From Base Require Export Free.Instance.Comb.
 From Base Require Export Free.Instance.Identity.
 From Base Require Export Free.Malias.
 From Base Require Export Free.Monad.
