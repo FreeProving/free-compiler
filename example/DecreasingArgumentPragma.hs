1bf402a4ed57354cc994bf137410c2a1d52c2523
Justin Andresen <jan.dresen.95@gmail.com>
2020-11-09 13:07:02 +0100

    Disable decreasing argument example #169
    The `mapRose` example does not work anymore in Coq because
    Coq does not know how `call` is implemented. Only when the
    concrete evaluation strategy is specified Coq can verify
    that the function terminates.

--- example/DecreasingArgumentPragma.hs
+++ example/DecreasingArgumentPragma.hs
@@ -4,18 +4,16 @@
 -- See @doc\/CustomPragmas\/DecreasingArgumentPragma.md@ for details.
 module DecreasingArgumentsPragma where
 
-map :: (a -> b) -> [a] -> [b]
-map f xs = case xs of
-  []        -> []
-  (x : xs') -> f x : map f xs'
+import           Data.List (map)
 
 data Rose a = Rose a [Rose a]
 
-{-# FreeC mapRose DECREASES ON r #-}
-mapRose :: (a -> b) -> Rose a -> Rose b
-mapRose f r = case r of
-  (Rose x rs) -> Rose (f x) (map (mapRose f) rs)
-
-{-# FreeC mapRose' DECREASES ON ARGUMENT 2 #-}
-mapRose' :: (a -> b) -> Rose a -> Rose b
-mapRose' f (Rose x rs) = Rose (f x) (map (mapRose' f) rs)
+-- {-# FreeC mapRose DECREASES ON r #-}
+-- {-# FreeC mapRose DECREASES ON r #-}
+-- mapRose :: (a -> b) -> Rose a -> Rose b
+-- mapRose f r = case r of
+--   Rose x rs -> Rose (f x) (map (mapRose f) rs)
+--
+-- {-# FreeC mapRose' DECREASES ON ARGUMENT 2 #-}
+-- mapRose' :: (a -> b) -> Rose a -> Rose b
+-- mapRose' f (Rose x rs) = Rose (f x) (map (mapRose' f) rs)
